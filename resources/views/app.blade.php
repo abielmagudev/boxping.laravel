1875e10f22cee1ee85f4bacfe329f4e6998017dd
abielmagu <abielmagu.dev@gmail.com>
2020-08-01 02:26:41 -0500

    Estructura basica de la plantilla principal de la app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>{{ config('app.name') }}</title>
 </head>
 <body>
-    
+    <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    </nav>
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
