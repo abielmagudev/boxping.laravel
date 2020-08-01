4d0b0fc6c45b7eb55eb561383e301f310374f953
abielmagu <abielmagu.dev@gmail.com>
2020-07-31 20:39:45 -0500

    Crear route y layout view  app para vistas

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>{{ config('app.name') }}</title>
+</head>
+<body>
+    
+</body>
+</html>
