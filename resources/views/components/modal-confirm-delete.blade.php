0f20cba077e8ff6b3087cec4b65355335127cf2c
abielmagu <abielmagu.dev@gmail.com>
2020-08-03 03:16:28 -0500

    Actualiza design y agrega feature para texto del button submit del componente modal-confirm-delete

--- resources/views/components/modal-confirm-delete.blade.php
+++ resources/views/components/modal-confirm-delete.blade.php
@@ -2,13 +2,14 @@
 $settings = (object) array(
     'route'   => isset($route) && is_string($route) ? $route : null,
     'content' => isset($content) ? $content : null,
+    'button'  => isset($button) && is_string($button) ? $button : 'Si, confirmo eliminar', 
 );
 ?>
 <div class="modal fade" id="confirmDeleteModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmar</h5>
+            <div class="modal-header bg-danger text-white">
+                <p class="modal-title" id="confirmDeleteModalLabel">ALERTA!</p>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -17,9 +18,11 @@ $settings = (object) array(
                 <form action="{{ $settings->route }}" method="post" class="text-center">
                     @method('delete')
                     @csrf
-                    {{ $settings->content }}
-                    <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Cancelar</button>
-                    <button type="submit" class="btn btn-outline-danger btn-sm border-0">Si, confirmo eliminar</button>
+                    <br>
+                    <div class="lead">{{ $settings->content }}</div>
+                    <br>
+                    <button type="button" class="btn btn-dark" data-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-outline-danger border-0">{{ $settings->button }}</button>
                 </form>
             </div>
             <div class="modal-footer d-none">
