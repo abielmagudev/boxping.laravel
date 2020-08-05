bce639294d9f0747161191afdc00721961352696
abielmagu <abielmagu.dev@gmail.com>
2020-08-05 01:58:58 -0500

    Modifica component modal-confirm-delete

--- resources/views/components/modal-confirm-delete.blade.php
+++ resources/views/components/modal-confirm-delete.blade.php
@@ -1,28 +1,35 @@
 <?php
 $settings = (object) array(
-    'route'   => isset($route) && is_string($route) ? $route : null,
-    'content' => isset($content) ? $content : null,
-    'button'  => isset($button) && is_string($button) ? $button : 'Si, confirmo eliminar', 
+    'trigger_text'  => isset($trigger_text) ? $trigger_text : 'Eliminar',
+    'trigger_align' => isset($trigger_align) ? $trigger_align : 'text-left',
+    'button_text'   => isset($button_text) ? $button_text : 'Si, confirmo eliminar', 
+    'action'        => $action,
+    'warning'       => $warning,
 );
 ?>
+
+<p class="{{ $settings->trigger_align }}">
+    <button data-toggle="modal" data-target="#confirmDeleteModal" class="btn btn-link btn-sm text-danger p-0">{{ $settings->trigger_text }}</button>
+</p>
+
 <div class="modal fade" id="confirmDeleteModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header bg-danger text-white">
-                <p class="modal-title" id="confirmDeleteModalLabel">ALERTA!</p>
+                <p class="modal-title" id="confirmDeleteModalLabel">Atención</p>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <form action="{{ $settings->route }}" method="post" class="text-center">
+                <form action="{{ $settings->action }}" method="post" class="text-center">
                     @method('delete')
                     @csrf
                     <br>
-                    <div class="lead">{{ $settings->content }}</div>
+                    <div class="lead">{{ $settings->warning }}</div>
                     <br>
                     <button type="button" class="btn btn-dark" data-dismiss="modal">Cancelar</button>
-                    <button type="submit" class="btn btn-outline-danger border-0">{{ $settings->button }}</button>
+                    <button type="submit" class="btn btn-outline-danger border-0">{{ $settings->button_text }}</button>
                 </form>
             </div>
             <div class="modal-footer d-none">
