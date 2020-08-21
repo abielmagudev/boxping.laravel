c7e4d4a95178011e0068e20e03f2c7c0d1aa21f1
abielmagu <abielmagu.dev@gmail.com>
2020-08-21 14:42:00 -0500

    Corrige bug en Userlive

--- app/Http/Controllers/Traits/Userlive.php
+++ app/Http/Controllers/Traits/Userlive.php
@@ -8,7 +8,7 @@ trait Userlive {
 
     protected function userlive()
     {
-        if( is_null($userlive) )
+        if( is_null($this->userlive) )
             $this->userlive = rand(1,10);
         
         return $this->userlive;
