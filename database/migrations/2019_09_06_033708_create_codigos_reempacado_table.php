e89c90b31b98c217c94851f744b4ed5546f631f9
abielmagu <abielmagu.dev@gmail.com>
2020-08-21 01:08:11 -0500

    Corrige la migracion el nombre de la tabla en funcion down

--- database/migrations/2019_09_06_033708_create_codigos_reempacado_table.php
+++ database/migrations/2019_09_06_033708_create_codigos_reempacado_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCodigosrTable extends Migration
+class CreateCodigosReempacadoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -28,6 +28,6 @@ class CreateCodigosrTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('codigosr');
+        Schema::dropIfExists('codigos_reempacado');
     }
 }
