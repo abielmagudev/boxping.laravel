57099102ab46872b991249cbc8f9099c77baee33
abielmagu <abielmagu.dev@gmail.com>
2020-09-10 16:58:31 -0500

    Cambios en la vista show de Consolidado

--- resources/views/consolidados/show/consolidado.blade.php
+++ resources/views/consolidados/show/consolidado.blade.php
@@ -5,49 +5,59 @@
                 <span>Consolidado </span>
             </div>
             <div>
-                <a href="{{ route('consolidados.edit', $consolidado->id) }}" class="btn btn-warning btn-sm">Editar</a>
+                @include('consolidados.includes.cerrado-badge')
+                <a href="{{ route('consolidados.edit', $consolidado->id) }}" class="btn btn-warning btn-sm">e</a>
             </div>
         </div>
     </div>
     <div class="card-body">
-        <p class="text-center">
-            @include('consolidados.includes.cerrado-badge')
-        </p>
         <div class="table-responsive">
-            <table class="table table-borderless table-sm small">
+            <table class="table table-borderless table-sm">
                 <tbody>
                     <tr>
-                        <td style="width:1%">Cliente</td>
+                        <td style="width:1%">
+                            <small class="text-muted">Cliente</small>
+                        </td>
                         <td>{{ $consolidado->cliente->nombre }}</td>
                     </tr>
                     <tr>
-                        <td>Número</td>
+                        <td>
+                            <small class="text-muted">Número</small>
+                        </td>
                         <td>{{ $consolidado->numero }}</td>
                     </tr>
                     <tr>
-                        <td>Tarimas</td>
+                        <td>
+                            <small class="text-muted">Tarimas</small>
+                        </td>
                         <td>
                             <span>{{ $consolidado->tarimas }}</span>
                         </td>
                     </tr>
                     <tr>
-                        <td>Notas</td>
+                        <td>
+                            <small class="text-muted">Notas</small>
+                        </td>
                         <td>
                             <p class="text-monospace small">{{ $consolidado->notas }}</p>
                         </td>
                     </tr>
                     <tr>
-                        <td>Creado</td>
                         <td>
+                            <small class="text-muted">Creado</small>
+                        </td>
+                        <td>
+                            <p class="m-0">{{ $consolidado->creator->name }}</p>
                             <p class="m-0">{{ $consolidado->created_at }}</p>
-                            <p class="m-0">{{ $consolidado->creado->name }}</p>
                         </td>
                     </tr>
                     <tr>
-                        <td>Actualizado</td>
                         <td>
+                            <small class="text-muted">Actualizado</small>
+                        </td>
+                        <td>
+                            <p class="m-0">{{ $consolidado->updater->name }}</p>
                             <p class="m-0">{{ $consolidado->updated_at }}</p>
-                            <p class="m-0">{{ $consolidado->actualizado->name }}</p>
                         </td>
                     </tr>
                 </tbody>
