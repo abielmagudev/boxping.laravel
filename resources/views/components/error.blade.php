bf5466901d70c398bf1ca2e0119decd1ca5e4aaa
abielmagu <abielmagu.dev@gmail.com>
2020-10-01 01:38:46 -0500

    Actualiza el componente de error

--- resources/views/components/error.blade.php
+++ resources/views/components/error.blade.php
@@ -1,7 +1,7 @@
 @if( $errors->any() )
 <div class="alert alert-warning" role="alert">
-    <p class="alert-heading lead">Hmm! Algo sucedio...</p>
-    <ul>
+    <p class="alert-heading">Revisa lo siguiente:</p>
+    <ul class="m-0">
         @foreach( $errors->all() as $message )
         <li>{{ $message }}</li>
         @endforeach
