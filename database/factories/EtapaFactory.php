cca8e48ec389542363ade638183058650e64b366
abielmagu <abielmagu.dev@gmail.com>
2020-10-05 02:17:09 -0500

    Refactoriza factory y views de Etapa

--- database/factories/EtapaFactory.php
+++ database/factories/EtapaFactory.php
@@ -9,15 +9,16 @@ $factory->define(Etapa::class, function (Faker $faker) {
 
     $nombre = 'Stage ' . $faker->unique(true)->randomNumber();
     $slug = str_replace(' ', '-', $nombre);
+    $realiza_medicion = $faker->boolean;
     $medidas_peso = config('system.measures.peso');
     $medidas_volumen = config('system.measures.volumen');
 
     return [
         'nombre' => $nombre,
         'slug' => strtolower($slug),
-        'realiza_medicion' => $faker->boolean ? 1 : 0,
-        'medida_peso' => $faker->randomElement($medidas_peso),
-        'medida_volumen' => $faker->randomElement($medidas_volumen),
+        'realiza_medicion' => $realiza_medicion ? 1 : 0,
+        'medida_peso' => $realiza_medicion ? $faker->randomElement($medidas_peso) : null,
+        'medida_volumen' => $realiza_medicion ? $faker->randomElement($medidas_volumen) : null,
         'created_by' => $faker->randomDigitNotNull,
         'updated_by' => $faker->randomDigitNotNull,
     ];
