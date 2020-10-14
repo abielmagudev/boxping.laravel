b99af9ff8306b65036afb63dea0800d4c537b0ee
abielmagu <abielmagu.dev@gmail.com>
2020-10-13 22:35:31 -0500

    Crea un wrapper para el contenido del componente notification

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -10,7 +10,7 @@ $notifications = array(
 @foreach($notifications as $notification => $color)
     @if( session()->has($notification) )
     <div class="alert {{ $color }} alert-dismissible fade show" role="alert">
-        <span><?= session($notification) ?></span>
+        <div id="notification-wrapper-content"><?= session($notification) ?></div>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
