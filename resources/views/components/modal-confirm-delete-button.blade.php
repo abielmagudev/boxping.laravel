b21c2d95251af29ad7640ea1833150c7ed7daaa3
abielmagu <abielmagu.dev@gmail.com>
2020-11-04 00:25:56 -0600

    Actualiza componente confirm-delete-button

--- resources/views/components/modal-confirm-delete-button.blade.php
+++ resources/views/components/modal-confirm-delete-button.blade.php
@@ -2,8 +2,9 @@
 
 $settings = (object) [
     'text' => isset($text) && is_string($text) ? $text : 'Eliminar',
+    'small' => isset($small) && is_bool($small) ? $small : false,
 ];
 
 ?>
 
-<button data-toggle="modal" data-target="#confirmDeleteModal" class="btn btn-link btn-sm text-danger p-0">{{ $settings->text }}</button>
+<button data-toggle="modal" data-target="#confirmDeleteModal" class="btn btn-link {{ $settings->small ? 'btn-sm' : '' }} text-danger p-0">{{ $settings->text }}</button>
