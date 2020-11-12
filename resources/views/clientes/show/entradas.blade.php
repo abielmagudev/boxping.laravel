22c84e73ec172bdfed55d1a64b186891ed5ed4b9
abielmagu <abielmagu.dev@gmail.com>
2020-11-12 14:02:25 -0600

    Implementa componente ultimas_entradas en view show de Cliente

--- resources/views/clientes/show/entradas.blade.php
+++ resources/views/clientes/show/entradas.blade.php
@@ -1,43 +1,4 @@
-<div class="card">
-    <div class="card-header">
-        <span><b>{{ $entradas_take }}</b> últimas entradas</span>
-    </div>
-    <div class="card-body p-0">
-        @if( $entradas->count() )
-        <div class="table-responsive">
-            <table class="table table-hover">
-                <thead class="small">
-                    <tr>
-                        <th>Número</th>
-                        <th>Destinatario</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $ultimas_entradas = $entradas->take( $entradas_take ) ?>
-                    @foreach($ultimas_entradas as $entrada)
-                    <tr>
-                        <td class="align-middle">
-                            <a href="{{ route('entradas.show', $entrada) }}">{{ $entrada->numero }}</a>
-                        </td>
-                        <td class="align-middle">
-                            @if( is_object($entrada->destinatario) )
-                            <span>{{ $entrada->destinatario->direccion }}, C.P. {{ $entrada->destinatario->codigo_postal }} <br> {{ $entrada->destinatario->localidad }}</span>
-                            
-                            @else
-                            <small class='text-muted'>PENDIENTE</small>
-                            
-                            @endif
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-
-        @else
-        <br>
-        <p class="text-center text-muted">Sin entradas</p>
-
-        @endif
-    </div>
-</div>
+@component('components.ultimas_entradas')
+    @slot('entradas', $entradas)
+    @slot('take', 10)
+@endcomponent
