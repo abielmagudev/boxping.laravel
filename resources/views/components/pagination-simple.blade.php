72ab8ff61c7e54a065aba0c90f51709d0a6b0357
abielmagu <abielmagu.dev@gmail.com>
2021-02-17 16:13:18 -0600

    Actualiza component pagination-simple

--- resources/views/components/pagination-simple.blade.php
+++ resources/views/components/pagination-simple.blade.php
@@ -1,23 +1,23 @@
 <?php
 
-$pagination_simple_sizes = array(
+$sizes = array(
     'sm' => 'pagination-sm',
     'lg' => 'pagination-lg',
 );
 
-$pagination_simple_aligns = array(
+$aligns = array(
     'center' => 'justify-content-center',
     'right'  => 'justify-content-end',
 );
 
-$pagination_simple_settings = (object) array(
-    'size'  => isset($size) && array_key_exists($size, $pagination_simple_sizes) ? $pagination_simple_sizes[$size] : '',
-    'align' => isset($align) && array_key_exists($align, $pagination_simple_aligns) ? $pagination_simple_aligns[$align] : '',
+$pagination = (object) array(
+    'size'  => isset($size) && array_key_exists($size, $sizes) ? $sizes[$size] : '',
+    'align' => isset($align) && array_key_exists($align, $aligns) ? $aligns[$align] : '',
 );
 
 ?>
 <div id="component-pagination-simple">
-    <ul class="pagination {{ $pagination_simple_settings->size }} {{ $pagination_simple_settings->align }}">
+    <ul class="pagination {{ $pagination->size }} {{ $pagination->align }}">
         <li class="page-item">
             @if( $collection->previousPageUrl() )
             <a href="{{ $collection->previousPageUrl() }}" class="page-link">Anterior</a>
