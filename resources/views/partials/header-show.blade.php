fbb46eb922be4c3ec437120c40af27aa5bb93bcb
abielmagu <abielmagu.dev@gmail.com>
2021-02-17 05:58:45 -0600

    Crea partial header show

---
+++ resources/views/partials/header-show.blade.php
@@ -0,0 +1,41 @@
+<?php 
+
+$header = (object) array(
+    'has_heading' => isset($pretitle) || isset($title) || isset($subtitle),
+    'has_options' => isset($options),
+    'options'     => isset($options) ? $options : false,
+    'pretitle'    => isset($pretitle) ? strval($pretitle) : false,
+    'title'       => isset($title) ? strval($title) : false,
+    'subtitle'    => isset($subtitle) ? strval($subtitle) : false,
+    'goback'      => isset($goback) ? strval($goback) : false,
+    'goback_text' => isset($goback_text) ? strval($goback_text) : 'Regresar',
+);
+
+?>
+
+<div class="row align-items-center">
+    @if( $header->has_heading )
+    <div class="col">
+        @if( $header->pretitle )
+        <small class="d-block small text-muted">{{ $header->pretitle }}</small>
+        @endif
+
+        <span>{{ $header->title }}</span>
+
+        @if( $header->subtitle )
+        <small class="d-block small text-muted">{{ $header->subtitle }}</small>
+        @endif
+    </div>
+    @endif
+
+    <div class="col text-end">
+        @if( $header->has_options ) 
+        <div class="d-inline-block">{{ $header->options }}</div>
+        @endif
+
+        @if( $header->goback )
+        <a href="{{ $header->goback }}" class="btn btn-sm btn-outline-secondary">{{ $header->goback_text }}</a>
+        @endif
+    </div>
+</div>
+<br>
