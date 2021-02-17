881c6f28cd69403cc91fac700f4476ef03430573
abielmagu <abielmagu.dev@gmail.com>
2021-02-17 05:58:19 -0600

    Crea partial table summary entradas

---
+++ resources/views/partials/table-summary-entradas.blade.php
@@ -0,0 +1,39 @@
+<?php
+
+$summary = (object) array(
+    'entradas' => isset($entradas) && $entradas instanceof \Illuminate\Database\Eloquent\Collection ? $entradas : false,
+);
+
+?>
+
+@if( ! is_bool($summary->entradas ) )
+
+    @component('components.table')
+        @slot('hover', true)
+        @slot('thead', ['Número','Destinatario',''])
+        @slot('tbody')
+        @foreach($summary->entradas as $entrada)
+        <tr>
+            <td style="min-width:288px">{{ $entrada->numero }}</td>
+            <td style="min-width:288px">
+                @if( $entrada->destinatario )
+                <span class="d-block">{{ $entrada->destinatario->direccion ?? '' }}</span>
+                <small>{{ $entrada->destinatario->localidad }}</small>
+
+                @else
+                <small class="text-muted">Pendiente</small>
+
+                @endif
+            </td>
+            <td class='text-end'>
+                <a href="{{ route('entradas.show', $entrada) }}" class="btn btn-sm btn-primary">
+                    @component('components.icon', ['icon' => 'eye'])
+                    @endcomponent
+                </a>
+            </td>
+        </tr>
+        @endforeach
+        @endslot
+    @endcomponent
+
+@endif
