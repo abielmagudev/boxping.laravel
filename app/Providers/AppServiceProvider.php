b586f9544a51c42e407ffd6c2f237aaf745f1d4d
abielmagu <abielmagu.dev@gmail.com>
2021-02-18 05:08:26 -0600

    Registra global config simbolos e iconos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::share('symbols', config('system.symbols'));
+        View::share('icons', config('system.icons-bs'));
     }
 }
