66e286dc90f8117cb2bcac858ecd1d0d9ca75800
abielmagu <abielmagu.dev@gmail.com>
2021-02-24 03:12:28 -0600

    Actualiza partial modal-delete

--- resources/views/partials/modal-confirm-delete.blade.php
+++ resources/views/partials/modal-confirm-delete.blade.php
@@ -27,39 +27,31 @@ $modal = (object) array(
     </button>
 </div>
 
-<!-- Modal -->
-<div class="modal fade" id="modalConfirmDelete" tabindex="-1" aria-labelledby="modalConfirmDeleteLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header border-0">
-                <p class="modal-title lead d-none" id="modalConfirmDeleteLabel">
-                    <i class="bi bi-exclamation-triangle"></i>
-                    <span class="align-middle ms-2">Confirmar</span>
-                </p>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body text-center">
-                <p class="display-1 text-danger">
-                    <i class="bi bi-exclamation-triangle-fill"></i>
-                </p>
-                <p class="display-6 mb-5">¿Deseas continuar?</p>
-                <form action="{{ $modal->route }}" method="post" id="form-confirm-delete">
-                    @csrf
-                    @method('delete')
-                    @if( ! is_bool($modal->body) )
-                    <div class='text-muted mb-4'>{!! $modal->body !!}</div>
-                    @endif
-                </form>
-            </div>
-            <div class="modal-footer justify-content-center bg-light">
-                <button type="submit" class="btn btn-danger" form="form-confirm-delete">Si, eliminar</button>
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-            </div>
-        </div>
-    </div>
-</div>
+@component('components.modal', [
+    'id' => 'modalConfirmDelete',
+    'centered' => true,
+])
+    @slot('body_classes', 'text-center')
+    @slot('body')
+    <p class="display-1 text-danger">
+        <i class="bi bi-exclamation-triangle-fill"></i>
+    </p>
+    <p class="display-6 mb-5">¿Deseas continuar?</p>
+    <form action="{{ $modal->route }}" method="post" id="form-confirm-delete">
+        @csrf
+        @method('delete')
+        @if( ! is_bool($modal->body) )
+        <div class='text-muted mb-4'>{!! $modal->body !!}</div>
+        @endif
+    </form>
+    @endslot
 
-@else
-<p class="text-muted">...ruta de eliminacion?</p>
+    @slot('footer_classes', 'bg-light justify-content-center')
+    @slot('footer')
+    <button type="submit" class="btn btn-danger" form="form-confirm-delete">Si, eliminar</button>
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+    @endslot
+
+@endcomponent
 
 @endif
