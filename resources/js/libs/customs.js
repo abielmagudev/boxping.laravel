3fff62d07cefbe2ecacc34539f9a95fd4aeab224
abielmagu <abielmagu.dev@gmail.com>
2021-02-25 05:10:25 -0600

    Activa popover de Bootstrap

--- resources/js/libs/customs.js
+++ resources/js/libs/customs.js
@@ -2,3 +2,8 @@ const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-tog
 const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
