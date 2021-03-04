85d2964f0552117ebefa0ff1cc86c249cf95735c
abielmagu <abielmagu.dev@gmail.com>
2021-03-04 00:08:06 -0600

    Crea partials subnav de salidas

---
+++ resources/views/partials/subnav-salidas.blade.php
@@ -0,0 +1,13 @@
+<div class="text-center">
+    @component('components.links-group', [
+        'links' => [
+            'salidas' => route('salidas.index'),
+            'transportadoras' => route('transportadoras.index'),
+            'incidentes' => route('incidentes.index'),
+        ],
+        'rounded_pill' => true,
+        'active' => isset($active) && is_string($active) ? $active : false,
+    ])
+    @endcomponent
+</div>
+<br>
