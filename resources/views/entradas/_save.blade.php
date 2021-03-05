a69d518e95bc5a3d920364564aec85c5708b05d9
abielmagu <abielmagu.dev@gmail.com>
2021-03-05 16:55:39 -0600

    Actualiza view save de Entrada

--- resources/views/entradas/_save.blade.php
+++ resources/views/entradas/_save.blade.php
@@ -12,7 +12,7 @@ if( ! is_null($entrada->id) )
 </div>
 <div class="mb-3">
     <label for="input-numero" class="form-label small">Número</label>
-    <input name="numero" value="{{ old('numero', $entrada->numero) }}" id="input-numero" type="text" class="form-control" required>
+    <input name="numero" value="{{ old('numero', $entrada->numero) }}" id="input-numero" type="text" class="form-control" autofocus required>
 </div>
 <div class="mb-3">
     <label for="textarea-contenido" class="form-label small">Contenido</label>
