a348a69e960e6da626e29b05a05f11f33b5b86c2
abielmagu <abielmagu.dev@gmail.com>
2021-03-06 17:34:29 -0600

    Crea nuevos main layouts offline y printing

---
+++ resources/views/offline.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>{{ config('app.name') }}</title>
+</head>
+<body>
+    <div class="container-fluid">
+        @yield('content')
+    </div>
+</body>
+</html>
