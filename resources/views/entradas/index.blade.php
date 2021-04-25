48fca41342ae1f4b2ed75b49baf5c19bb2eeb529
abielmagu <abielmagu.aio@gmail.com>
2021-04-25 03:41:31 -0500

    Agrega opciones de impresion en index Entrada

--- resources/views/entradas/index.blade.php
+++ resources/views/entradas/index.blade.php
@@ -1,6 +1,21 @@
 @extends('app')
 @section('content')
 
+<div class="text-end">
+    <form action="{{ route('printing.entradas') }}" method="get" id="form-print-list" target="_blank"></form>
+    <div class="dropdown">
+        <button class="btn btn-outline-primary btn-sm dropdown-toggle" type="button" id="dropdownPrintMenu" data-bs-toggle="dropdown" aria-expanded="false">
+            Imprimir selección
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownPrintMenu">
+            <li><button class="dropdown-item" type="submit" form="form-print-list">Informacion</button></li>
+            <li><button class="dropdown-item" type="submit" name="hoja" value="etiqueta" form="form-print-list">Etiquetas</button></li>
+            <li><button class="dropdown-item" type="submit" name="hoja" value="etapas" form="form-print-list">Etapas</button></li>
+        </ul>
+    </div>
+</div>
+<br>
+
 @component('components.card')
     @slot('header_title', 'Entradas')
     @slot('header_title_badge', $entradas->total())
@@ -11,10 +26,13 @@
     @endslot
     @slot('body')
     @component('components.table')
-        @slot('thead', ['Número','Consolidado','Cliente','Destinatario'])
+        @slot('thead', ['','Número','Consolidado','Cliente','Destinatario'])
         @slot('tbody')
         @foreach($entradas as $entrada)
         <tr>
+            <td style="width:1%">
+                <input type="checkbox" name="lista[]" value="{{ $entrada->id }}" id="checkbox-list" class="form-check-input" form="form-print-list">
+            </td>
             <td>{{ $entrada->numero }}</td>
             <td>
                 @if( $entrada->consolidado )
