8bd5b2b4f2378a4510da4d9f4848d9c387d80d0d
abielmagu <abielmagu.aio@gmail.com>
2021-06-02 04:03:02 -0500

    Implementa la funcion asset para los recursos de css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bs-css/bootstrap.min.css">
-    <link rel="stylesheet" href="bs-css/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{ asset('bs-css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('bs-css/bootstrap-icons.css') }}">
     <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
     <title>{{ config('app.name') }}</title>
 </head>
