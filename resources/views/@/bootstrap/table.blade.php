ec3f12422e0aff44058fda4e3f4f9b12d6368395
abielmagu <abielmagu.aio@gmail.com>
2021-06-11 19:26:40 -0500

    Actualiza componente table

--- resources/views/@/bootstrap/table.blade.php
+++ resources/views/@/bootstrap/table.blade.php
@@ -1,16 +1,23 @@
 <?php
 
 $settings = (object) [
-    'thead' => $thead ?? false,
-    'tbody' => $tbody ?? null,
-    'has_thead' => isset($thead) && is_array($thead),
+    'bordered' => isset($bordered) && $bordered === true ? 'table-bordered' : '',
+    'border_color' => isset($border_color) && is_string($border_color) ? "border-{$border_color}" : '',
+    'borderless' => isset($borderless) && $borderless === true ? 'table-borderless' : '',
+    'color' => isset($color) && is_string($color) ? "table-{$color}" : '',
     'has_tbody' => isset($tbody),
+    'has_thead' => isset($thead) && is_array($thead),
+    'hover' => isset($hover) && $hover === false ? '' : 'table-hover',
+    'small' => isset($small) && $small === true ? 'table-sm' : '',
+    'striped' => isset($striped) && $striped === true ? 'table-striped' : '',
+    'tbody' => $tbody ?? null,
+    'thead' => $thead ?? false,
 ];
 
 ?>
 
 <div class="table-responsive">
-    <table class="table table-hover align-middle">
+    <table class="table {{ $settings->bordered }} {{ $settings->border_color }} {{ $settings->borderless }} {{ $settings->color }} {{ $settings->hover }} {{ $settings->small }} {{ $settings->striped }} align-middle">
         @if( $settings->has_thead )    
         <thead>
             <tr class="">
