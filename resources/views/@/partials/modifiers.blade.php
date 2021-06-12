d3a0f48b6915fdc33040c96b138d36b30d94882f
abielmagu <abielmagu.aio@gmail.com>
2021-06-11 19:28:51 -0500

    Actualiza partial de modifiers, cambia de entity a model

--- resources/views/@/partials/modifiers.blade.php
+++ resources/views/@/partials/modifiers.blade.php
@@ -1,24 +1,26 @@
 <?php
 
 $settings = (object) [
-    'entity' => $entity ?? new stdClass,
-    'has_entity' => isset($entity) && is_object($entity),
+    'model' => $model ?? new stdClass,
+    'has_model' => isset($model) && is_object($model),
     'show_updated' => isset($show_updated) && is_bool($show_updated) ? $show_updated : true,
     'show_created' => isset($show_created) && is_bool($show_created) ? $show_created : true,
 ];
 
 ?>
 
+@if( $settings->has_model )
 <div class="text-center text-uppercase text-muted" style="font-size:0.6rem">
     @if( $settings->show_updated )
-    <span class="me-1">Actualizado {{ $settings->entity->updated_at }}, {{ $settings->entity->updater->name }}</span>
+    <span class="me-1">Actualizado {{ $settings->model->updated_at }}, {{ $settings->model->updater->name }}</span>
     @endif
 
-    @if( $settings->show_updated  )
+    @if( $settings->show_updated && $settings->show_created )
     <span class="d-none d-md-inline-block">|</span>
     @endif
 
-    @if( $settings->show_created && $settings->show_created )
-    <span class="ms-1">Creado {{ $settings->entity->created_at }}, {{ $settings->entity->creator->name }}</span>
+    @if( $settings->show_created )
+    <span class="ms-1">Creado {{ $settings->model->created_at }}, {{ $settings->model->creator->name }}</span>
     @endif
 </div>
+@endif
