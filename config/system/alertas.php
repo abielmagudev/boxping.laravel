baef5da29bf864a00c35ea3486d6b800875ae9b0
abielmagu <abielmagu.aio@gmail.com>
2021-06-11 23:48:26 -0500

    Actualiza colores de alertas

--- config/system/alertas.php
+++ config/system/alertas.php
@@ -3,16 +3,16 @@
 // nivel => [color, descripcion] - props
 
 return [
-    'bajo' => [
-        'color' => '#FFAB06',
-        'descripcion' => 'No es necesario internvenir ni detener el proceso.',
+    'alto' => [
+        'color' => '#FE0606',
+        'descripcion' => 'Detener el proceso e intervenir para una solucíon.',
     ],
     'medio' => [
-        'color' => '#FF7806',
+        'color' => '#FE7806',
         'descripcion' => 'Intervenir para solucionar pero no detener el proceso.',
     ],
-    'alto' => [
-        'color' => '#FF060D',
-        'descripcion' => 'Detener el proceso e intervenir para una solucíon.',
+    'bajo' => [
+        'color' => '#FEC006',
+        'descripcion' => 'No es necesario internvenir ni detener el proceso.',
     ],
 ];
