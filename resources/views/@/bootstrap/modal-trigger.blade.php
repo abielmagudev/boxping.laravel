a9936f4f42841ce4a67c9fd36389cd45fbd51ac9
abielmagu <abielmagu.aio@gmail.com>
2021-06-18 16:28:17 -0500

    Actualiza componente modal-trigger de Bs

--- resources/views/@/bootstrap/modal-trigger.blade.php
+++ resources/views/@/bootstrap/modal-trigger.blade.php
@@ -1,28 +1,31 @@
 <?php
 
-$settings = (object) [
-    'is_link' => isset($is_link) && is_bool($is_link) ? $is_link : false,
-    'classes' => isset($classes) && is_string($classes) ? $classes : null,
-    'text' => isset($text) && is_string($text) ? $text : 'Modal Trigger',
-    'modal' => "#{$modal}" ?? null,
-    'data' => null,
-];
-
-if( isset($data) && is_array($data) )
+function getDataAttributes($data)
 {
+    $attributes = [];
     foreach($data as $key => $value)
     {
         if(! is_numeric($key) )
-            $settings->data .= " data-{$key}=\"{$value}\" ";
+            array_push($attributes, "{$key}=\"{$value}\"") ;
     }
+
+    return implode(' ', $attributes);
 }
+
+$settings = (object) [
+    'classes' => isset($classes) && is_string($classes) ? $classes : null,
+    'is_link' => isset($is_link) && is_bool($is_link) ? $is_link : false,
+    'modal_id' => "#{$modal_id}" ?? '#modal-idless',
+    'dataset' => isset($data) && is_array($data) ? getDataAttributes($data) : '',
+    'text' => isset($text) && is_string($text) ? $text : 'Modal trigger',
+];
 
 ?>
 
 @if( $settings->is_link )
-    <a href="#!" data-bs-toggle="modal" data-bs-target="{{ $settings->modal }}" class="{{ $settings->classes }}" {!! $settings->data !!}>{!! $settings->text !!}</a>
+    <a href="#!" data-bs-toggle="modal" data-bs-target="{{ $settings->modal_id }}" class="{{ $settings->classes }}" {!! $settings->dataset !!}>{!! $settings->text !!}</a>
 
 @else
-    <button type="button" data-bs-toggle="modal" data-bs-target="{{ $settings->modal }}" class="{{ $settings->classes }}" {!! $settings->data !!}>{!! $settings->text !!}</button>
+    <button type="button" data-bs-toggle="modal" data-bs-target="{{ $settings->modal_id }}" class="{{ $settings->classes }}" {!! $settings->dataset !!}>{!! $settings->text !!}</button>
 
 @endif
