21dace03cdc15f25e1347bbd62c9e2af60a918a9
abielmagu <abielmagu.aio@gmail.com>
2021-06-18 06:03:27 -0500

    Componente pagination-simple de Bs

---
+++ resources/views/@/bootstrap/pagination-simple.blade.php
@@ -0,0 +1,48 @@
+<?php
+
+use \Illuminate\Support\Facades\Route;
+
+$sizes = array(
+    'sm' => 'pagination-sm',
+    'lg' => 'pagination-lg',
+);
+
+$aligns = array(
+    'center' => 'justify-content-center',
+    'right'  => 'justify-content-end',
+);
+
+$settings = (object) array(
+    'size'  => isset($size) && array_key_exists($size, $sizes) ? $sizes[$size] : '',
+    'align' => isset($align) && array_key_exists($align, $aligns) ? $aligns[$align] : '',
+    'has_prev' => isset($prev) && Route::has($prev),
+    'has_next' => isset($next) && Route::has($next),
+    'route_prev' => $prev ?? false,
+    'route_next' => $next ?? false,
+    'text_prev' => isset($next_text) && is_string($next_text) ? $next_text : 'Siguiente',
+    'text_next' => isset($prev_text) && is_string($prev_text) ? $prev_text : 'Anterior',
+);
+
+?>
+<div id="wrapper-pagination-simple">
+    <ul class="pagination {{ $settings->size }} {{ $settings->align }}">
+        <li class="page-item">
+            @if( $settings->has_prev )
+            <a href="{{ $settings->route_prev }}" class="page-link">{{ $settings->text_prev }}</a>
+
+            @else
+            <span class="page-link text-muted">{{ $settings->text_prev }}</span>
+
+            @endif
+        </li>
+        <li class="page-item">
+            @if( $settings->has_next )
+            <a href="{{ $settings->route_next }}" class="page-link">{{ $settings->text_next }}</a>
+
+            @else
+            <span class="page-link text-muted">{{ $settings->text_next }}</span>
+
+            @endif
+        </li>
+    </ul>
+</div>
