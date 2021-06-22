12f719c4b57a9d1f56a3736bf3d5187d368ac0e3
abielmagu <abielmagu.aio@gmail.com>
2021-06-22 03:44:50 -0500

    Agrega helper personalizado de replazador de caracteres especiales

--- app/Helpers/text.php
+++ app/Helpers/text.php
@@ -1,9 +1,38 @@
 <?php
 
-if(! function_exists('capitalize') )
+if( ! function_exists('capitalize') )
 {
     function capitalize($text)
     {
         return ucwords( strtolower($text) );
+    }
+}
+
+if( ! function_exists('replaceSpecialChars') )
+{
+    function replaceSpecialChars($string)
+    {
+        $utf8 = [
+            '/[áàâãªä]/u' => 'a',
+            '/[ÁÀÂÃÄ]/u'  => 'A',
+            '/[ÍÌÎÏ]/u'   => 'I',
+            '/[íìîï]/u'   => 'i',
+            '/[éèêë]/u'   => 'e',
+            '/[ÉÈÊË]/u'   => 'E',
+            '/[óòôõºö]/u' => 'o',
+            '/[ÓÒÔÕÖ]/u'  => 'O',
+            '/[úùûü]/u'   => 'u',
+            '/[ÚÙÛÜ]/u'   => 'U',
+            '/ç/'         => 'c',
+            '/Ç/'         => 'C',
+            '/ñ/'         => 'n',
+            '/Ñ/'         => 'N',
+            '/–/'         => '-', // UTF-8 hyphen to "normal" hyphen
+            '/[’‘‹›‚]/u'  => '', // Literally a single quote
+            '/[“”«»„]/u'  => '', // Double quote
+            '/ /'         => '', // nonbreaking space (equiv. to 0x160)
+        ];
+    
+        return  preg_replace( array_keys($utf8), array_values($utf8), $string);
     }
 }
