fd9094d7c05e9def4a4e4090d09f0963b89c94e7
abielmagu <abielmagu.aio@gmail.com>
2021-06-22 05:29:10 -0500

    Actualiza componente card

--- resources/views/@/bootstrap/card.blade.php
+++ resources/views/@/bootstrap/card.blade.php
@@ -1,20 +1,21 @@
 <?php 
 
 $settings = (object) [
-    'body' => $body ?? null,
-    'footer' => $footer ?? null,
-    'header' => $header ?? null,
+    'body_padding' => isset($body_padding) && $body_padding === false ? 'p-0' : '',
+    'body' => $body ?? '',
+    'footer' => $footer ?? '',
     'has_body' => isset($body),
     'has_footer' => isset($footer),
     'has_header' => isset($header),
-    'body_padding' => isset($body_padding) && $body_padding === false ? 'p-0' : '',
+    'header_color' => isset($header_color) && is_string($header_color) ? $header_color : '',
+    'header' => $header ?? '',
 ];
 
 ?>
 
 <div class="card">
     @if( $settings->has_header )
-    <div class="card-header">
+    <div class="card-header {{ $settings->header_color }}">
         {!! $settings->header !!}
     </div>
     @endif
