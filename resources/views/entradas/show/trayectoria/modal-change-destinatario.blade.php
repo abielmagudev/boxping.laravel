592f24264ffce93afdc03615e0b2d2e6d4bdfa50
abielmagu <abielmagu.aio@gmail.com>
2021-06-27 18:19:41 -0500

    Agrega modals de busqueda en show-trayectoria de Entrada

---
+++ resources/views/entradas/show/trayectoria/modal-change-destinatario.blade.php
@@ -0,0 +1,15 @@
+<!-- Modal destinatarios search -->
+@component('@.bootstrap.modal', [
+    'header_classes' => 'border-0',
+    'header_close' => true,
+    'id' => 'modalSearchToChangeDestinatario',
+    'title' => 'Buscar destinatarios',
+])
+    @slot('body')
+    <form action="{{ route('entradas.edit', $entrada) }}" method="get" autocomplete="off">
+        <input type="hidden" name="editor" value="destinatario">
+        <input type="search" name="buscar" placeholder="Escribe el nombre, dirección, teléfono..." id="inputBuscarDestinatarios" class="form-control rounded-pill" required>
+    </form>
+    <br>
+    @endslot
+@endcomponent
