221a06df291cadd9dec7a5d4023845175e7656d1
abielmagu <abielmagu.aio@gmail.com>
2021-07-03 05:58:17 -0500

    Agrega funcionalidad de Js para modificar la ruta del formulario de impresion

--- resources/views/@/partials/sheets-printing-dropdown.blade.php
+++ resources/views/@/partials/sheets-printing-dropdown.blade.php
@@ -2,21 +2,35 @@
 
 $sheets_printing = config('system.impresion.hojas');
 $sheets_except = isset($except) && is_array($except) ? $except : [];
-$sheets = array_diff($sheets_printing, $sheets_except);
-$form_printing_id = 'formEntradasPrinting';
+$sheets_only = array_diff($sheets_printing, $sheets_except);
 
 ?>
 
 <div class="dropdown d-inline-block">
-    <button class="btn btn-primary btn-sm dropdown-toggle" type="button" id="dropdownSheetsPrinting" data-bs-toggle="dropdown" aria-expanded="false">
+    <button class="btn btn-primary btn-sm dropdown-toggle" type="button" id="dropdownTemplatesPrinting" data-bs-toggle="dropdown" aria-expanded="false">
         <span class="me-1">{!! $svg->printer_fill !!}</span>
     </button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownSheetsPrinting">
-        @foreach ($sheets as $sheet)  
+    <ul class="dropdown-menu" aria-labelledby="dropdownTemplatesPrinting">
+        @foreach ($sheets_only as $sheet)  
         <li>
-            <button type="submit" class="dropdown-item" name="hoja" value="{{ $sheet }}" form="{{ $form_printing_id }}">{{ ucfirst($sheet) }}</button>
+            <button type="button" class="dropdown-item" form="formEntradasPrinting" name="hoja" value="{{ $sheet }}">{{ ucfirst($sheet) }}</button>
         </li>
         @endforeach
     </ul>
-    <form action="{{ route('printing.entradas') }}" method="get" id="{{ $form_printing_id }}"></form>
+    <form action="{{ route('entradas.printing.multiple') }}" method="get" id="formEntradasPrinting"></form>
 </div>
+
+<script>
+
+let buttonsPrinting = document.querySelectorAll('button[form="formEntradasPrinting"]'); 
+let formPrinting = document.getElementById('formEntradasPrinting');
+let actionForm = formPrinting.action;
+
+buttonsPrinting.forEach( function (item, index) {
+    item.addEventListener('click', function (e) {
+        formPrinting.action = actionForm + "/" + item.value;
+        formPrinting.submit();
+    })
+});
+
+</script>
