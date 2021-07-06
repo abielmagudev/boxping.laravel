1b38aa043dbe08bff04a5423ae77e7c3275b7c40
abielmagu <abielmagu.aio@gmail.com>
2021-07-05 22:38:07 -0500

    Actualiza partial de impresion singular

--- resources/views/@/partials/entradas-printing/single-sheets-dropdown.blade.php
+++ resources/views/@/partials/entradas-printing/single-sheets-dropdown.blade.php
@@ -1,9 +1,9 @@
 <div class="d-inline dropdown">
-    <button class="btn btn-sm btn-primary dropdown-toggle" type="button" id="dropdownMenuPrint" data-bs-toggle="dropdown" aria-expanded="false">
+    <button class="btn btn-sm btn-primary dropdown-toggle" type="button" id="dropdownEntradaPrinting" data-bs-toggle="dropdown" aria-expanded="false">
         <span class="d-inline-block d-md-none me-1">{!! $svg->printer_fill !!}</span>
         <span class="d-none d-md-inline-block me-1">Imprimir</span>
     </button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownMenuPrint">
+    <ul class="dropdown-menu" aria-labelledby="dropdownEntradaPrinting">
         @foreach ($printing_sheets as $sheet)
         <li>
             <a class="dropdown-item" href="{{ route('entradas.printing',[$entrada,'hoja' => $sheet]) }}">{{ ucfirst($sheet) }}</a>
