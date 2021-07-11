961c9ec8fe068bff5ef26e9c477fdae74d458879
abielmagu <abielmagu.aio@gmail.com>
2021-07-11 17:36:51 -0500

    Implemente componente table en actualizaciones de Entrada

--- resources/views/entradas/show/proceso/actualizaciones.blade.php
+++ resources/views/entradas/show/proceso/actualizaciones.blade.php
@@ -3,18 +3,21 @@
 $counter = $actualizaciones->count();
 
 ?>
-<div class="tab-pane fade" style="max-height:400px" id="actualizaciones" role="tabpanel" aria-labelledby="actualizaciones-tab">
-    <div class="table-responsive">
-        <table class="table table-striped table-hover table-sm small">
-            <tbody>
-                @foreach($actualizaciones as $actualizacion)
-                <tr>
-                    <td class="text-muted">{{ $counter-- }}</td>
-                    <td class="fst-italic">{{ $actualizacion->updater->name ?? 'Guest' }} {{ $actualizacion->descripcion }}</td>
-                    <td class="">{{ $actualizacion->created_at }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+<div class="tab-pane fade" style="max-height:380px;" id="actualizaciones" role="tabpanel" aria-labelledby="actualizaciones-tab">
+        @component('@.bootstrap.table', [
+            'striped' => true,
+            'small' => true,
+            'classes' => 'small',
+            'thead' => ['No.','Descripción', 'Fecha'],
+        ])
+            @slot('tbody')
+            @foreach($actualizaciones as $actualizacion)
+            <tr class="align-top">
+                <td class="text-muted">{{ $counter-- }}</td>
+                <td class="fst-italic">{{ $actualizacion->updater->name ?? 'Guest' }} {{ $actualizacion->descripcion }}</td>
+                <td class="">{{ $actualizacion->created_at }}</td>
+            </tr>
+            @endforeach
+            @endslot
+        @endcomponent
 </div>
