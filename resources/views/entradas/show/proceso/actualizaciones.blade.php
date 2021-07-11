27b5c9ccf034d1900c2a8ff0da59061b0a2f88fd
abielmagu <abielmagu.aio@gmail.com>
2021-07-11 17:23:10 -0500

    Actualiza content de actualizaciones con overflow en Entrada

--- resources/views/entradas/show/proceso/actualizaciones.blade.php
+++ resources/views/entradas/show/proceso/actualizaciones.blade.php
@@ -3,17 +3,15 @@
 $counter = $actualizaciones->count();
 
 ?>
-<div class="tab-pane fade overflow-auto h-100" id="actualizaciones" role="tabpanel" aria-labelledby="actualizaciones-tab">
+<div class="tab-pane fade" style="max-height:400px" id="actualizaciones" role="tabpanel" aria-labelledby="actualizaciones-tab">
     <div class="table-responsive">
         <table class="table table-striped table-hover table-sm small">
             <tbody>
                 @foreach($actualizaciones as $actualizacion)
                 <tr>
                     <td class="text-muted">{{ $counter-- }}</td>
-                    <td>
-                        <div class="fst-italic mb-2">{{ $actualizacion->updater->name ?? 'Guest' }} {{ $actualizacion->descripcion }}</></div>
-                        <div class="text-muted text-end">{{ $actualizacion->created_at }}</div>
-                    </td>
+                    <td class="fst-italic">{{ $actualizacion->updater->name ?? 'Guest' }} {{ $actualizacion->descripcion }}</td>
+                    <td class="">{{ $actualizacion->created_at }}</td>
                 </tr>
                 @endforeach
             </tbody>
