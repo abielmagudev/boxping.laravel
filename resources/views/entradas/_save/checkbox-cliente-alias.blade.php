47c47a73a8158bcbff5115b0b23ebcf5dd997545
abielmagu <abielmagu.aio@gmail.com>
2021-07-23 17:56:29 -0500

    Elimina archivo include de cliente-alias en Entrada

--- resources/views/entradas/_save/checkbox-cliente-alias.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="form-check mb-3">
-    <input id="checkboxClienteAliasNumero" class="form-check-input" name="cliente_alias_numero" value="1" type="checkbox" {{ toggleChecked(1, old('cliente_alias_numero', $entrada->cliente_alias_numero)) }}>
-    <label for="checkboxClienteAliasNumero" class="form-check-label small">Alias del cliente al comienzo del número de entrada.</label>
-</div>
