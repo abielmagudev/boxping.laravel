8a9772be380849b3a359fe6dfba3896a4ac2c30d
abielmagu <abielmagu.aio@gmail.com>
2021-07-26 01:45:20 -0500

    Actualiza y factoriza nuevas funciones en Counter de Entrada

--- app/Ahex/Entrada/Application/Counter.php
+++ app/Ahex/Entrada/Application/Counter.php
@@ -6,21 +6,53 @@ use Illuminate\Support\Collection;
 
 abstract class Counter
 {
+    private static $result = [];
+
     public static function byConductor(Collection $entradas)
     {
-        $result = [];
-
         foreach($entradas as $entrada)
         {
-            if( ! array_key_exists($entrada->conductor_id, $result) )
+            if( ! array_key_exists($entrada->conductor_id, self::$result) )
             {
-                $result[$entrada->conductor_id] = $entrada->conductor;
-                $result[$entrada->conductor_id]->entradas = collect();
+                self::$result[$entrada->conductor_id] = $entrada->conductor;
+                self::$result[$entrada->conductor_id]->entradas = collect();
             }
             
-            $result[$entrada->conductor_id]->entradas->push($entrada);
+            self::$result[$entrada->conductor_id]->entradas->push($entrada);
         }
 
-        return $result;
+        return self::$result;
+    }
+
+    public static function byReempacador(Collection $entradas)
+    {
+        foreach($entradas as $entrada)
+        {
+            if( ! array_key_exists($entrada->reempacador_id, self::$result) )
+            {
+                self::$result[$entrada->reempacador_id] = $entrada->reempacador;
+                self::$result[$entrada->reempacador_id]->entradas = collect();
+            }
+            
+            self::$result[$entrada->reempacador_id]->entradas->push($entrada);
+        }
+
+        return self::$result;
+    }
+
+    public static function byCodigor(Collection $entradas)
+    {
+        foreach($entradas as $entrada)
+        {
+            if( ! array_key_exists($entrada->codigor_id, self::$result) )
+            {
+                self::$result[$entrada->codigor_id] = $entrada->codigor;
+                self::$result[$entrada->codigor_id]->entradas = collect();
+            }
+            
+            self::$result[$entrada->codigor_id]->entradas->push($entrada);
+        }
+
+        return self::$result;
     }
 }
