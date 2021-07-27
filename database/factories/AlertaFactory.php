df4eee6b1ccb9b2928fb2c69ab65bcbb1d3d29c3
abielmagu <abielmagu.aio@gmail.com>
2021-07-27 04:11:55 -0500

    Actualiza factories para evitar duplicados

--- database/factories/AlertaFactory.php
+++ database/factories/AlertaFactory.php
@@ -10,7 +10,7 @@ $niveles = array_keys( config('system.alertas') );
 $factory->define(Alerta::class, function (Faker $faker) use ($niveles) {
 
     $alerta_nivel = $faker->randomElement( $niveles );
-    $alerta_nombre = ucfirst($alerta_nivel) . ' ' . $faker->unique(true)->numberBetween(1,21);
+    $alerta_nombre = ucfirst($alerta_nivel) . $faker->unique(true)->numberBetween(1,20) . chr(rand(65,90));
 
     return [
         'nivel' => $alerta_nivel,
--- database/factories/VehiculoFactory.php
+++ database/factories/VehiculoFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Vehiculo::class, function (Faker $faker) {
     return [
-        'nombre' => 'Vehiculo #' . $faker->unique(true)->randomNumber,
+        'nombre' => 'Vehiculo #' . $faker->unique(true)->randomNumber . chr(rand(65,90)),
         'descripcion' => $faker->text(),
         'created_by' => rand(1,10),
         'updated_by' => rand(1,10),
