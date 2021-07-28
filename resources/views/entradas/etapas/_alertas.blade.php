b0f9a65b12293a588bc7e628147aa6f2f9ae6a17
abielmagu <abielmagu.aio@gmail.com>
2021-07-28 02:50:09 -0500

    Actualiza checkboxes alertas de etapa en Entrada

--- resources/views/entradas/etapas/_alertas.blade.php
+++ resources/views/entradas/etapas/_alertas.blade.php
@@ -1,19 +1,22 @@
 @if( $alertas->count() )
-<label class="form-label small">Alertas</label>
-<div class="border rounded p-3">
-    <div class="">
-        @foreach($alertas as $alerta)
-        <?php $alerta_checked = in_array($alerta->id, $attached_alertas) ? 'checked' : '' ?>
-
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" name="alertas[]" value="{{ $alerta->id }}" id="checkbox-alerta-{{ $alerta->id }}" style="border: 2px solid {{ $alerta->color }};" {{ $alerta_checked }}>
-            
-            <label class="form-check-label" for="checkbox-alerta-{{ $alerta->id }}">
-                <span>{{ $alerta->nombre }}</span>
-                <small class="d-none">{{ $alerta->descripcion }}</small>
-            </label>
-        </div>
-        @endforeach
+<label class="form-label small">
+    <span>Alertas</span>
+    <span class="badge bg-primary">{{ count($attached_alertas) }}</span>
+</label>
+<div class="border rounded overflow-auto" style="height:33vh">
+        <ul class="list-group list-group-flush">
+            @foreach($alertas as $alerta)
+            <?php $alerta_checkbox_id = "checkbox-alerta-{$alerta->id}" ?>
+            <?php $alerta_checked = in_array($alerta->id, $attached_alertas) ? 'checked' : '' ?>
+            <li class="list-group-item list-group-item-action d-flex">
+                <div class="me-3">
+                    <input class="form-check-input" type="checkbox" id="{{ $alerta_checkbox_id }}" name="alertas[]" value="{{ $alerta->id }}" {{ $alerta_checked }}>
+                </div>
+                <div class="">
+                    <label for="{{ $alerta_checkbox_id }}"> {{ $alerta->nombre }}</label>
+                </div>
+            </li>
+            @endforeach
+        </ul>
     </div>
-</div>
 @endif
