943478d942787a71e69bfbbcc219f7226c3ad34a
abielmagu <abielmagu.aio@gmail.com>
2021-07-28 03:14:26 -0500

    Actualiza trait de condiciones de Entrada

--- app/Ahex/Entrada/Domain/ConditionsTrait.php
+++ app/Ahex/Entrada/Domain/ConditionsTrait.php
@@ -2,10 +2,27 @@
 
 namespace App\Ahex\Entrada\Domain;
 
+use App\Destinatario;
+
 Trait ConditionsTrait
 {
     public function hasConsolidado()
     {
         return (bool) ! is_null($this->consolidado_id);
+    }
+
+    public function haveDestinatario()
+    {
+        return (bool) Destinatario::withTrashed()->where('id', $this->destinatario_id)->exists();
+    }
+
+    public function haveConfirmacion()
+    {
+        return (bool) is_string($this->confirmado_at);
+    }
+
+    public function haveSalida()
+    {
+        return (bool) isset($this->salida);
     }
 }
