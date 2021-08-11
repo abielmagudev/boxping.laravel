336a8306439b88c7457904bda7041522618f2293
abielmagu <abielmagu.aio@gmail.com>
2021-08-11 02:02:24 -0500

    Crea config tipografias

---
+++ config/system/tipografias.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'fuentes' => [
+        'arial' => 'Arial',
+        'courier' => 'Courier',
+        'helvetica' => 'Helvetica',
+        'times new roman' => 'Times New Roman',
+        'verdana' => 'Verdana',
+    ],
+    'mediciones' => [
+        'px' => 'Pixeles',
+        'pt' => 'Puntos',
+        'em' => 'Ems',
+        'pc' => '%',
+    ],
+    'interlineados' => range(0.5, 2.5, 0.5),
+];
