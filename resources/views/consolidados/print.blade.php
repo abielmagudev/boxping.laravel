79832ce2aee1dc720875dc9e8f34593cc8cd21a2
abielmagu <abielmagu.aio@gmail.com>
2021-09-01 04:33:27 -0500

    Corrige extends para impresion de Consolidado

--- resources/views/consolidados/print.blade.php
+++ resources/views/consolidados/print.blade.php
@@ -1,6 +1,6 @@
-@extends('printing')
+@extends('print')
     @section('header')
-    <div class="sticky-top text-center d-print-none py-3 bg-secondary mw-100">
+    <div class="sticky-top text-center d-print-none py-1 bg-secondary mw-100">
         <a href="{{ route('consolidados.show',$consolidado) }}" class="btn btn-secondary">Regresar</a>
     </div>
     @endsection
