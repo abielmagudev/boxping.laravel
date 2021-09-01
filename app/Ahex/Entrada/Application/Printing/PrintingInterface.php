cfab1c05da6b72e326a8068bffe118957e0921cd
abielmagu <abielmagu.aio@gmail.com>
2021-09-01 04:20:06 -0500

    Elimina sistema de impresion pasado de Entrada

--- app/Ahex/Entrada/Application/Printing/PrintingInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Ahex\Entrada\Application\Printing;
-
-interface PrintingInterface
-{
-    public function content($entrada);
-    public function sheet();
-}
--- resources/views/entradas/printing/single.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('printing')
-@section('header')
-    <div class="sticky-top text-center d-print-none py-3 bg-secondary mw-100">
-        <a href="{{ route('entradas.show',$entrada) }}" class="btn btn-secondary">Regresar</a>
-    </div>
-@endsection
-@section('content')
-    @include($sheet)
-@endsection
