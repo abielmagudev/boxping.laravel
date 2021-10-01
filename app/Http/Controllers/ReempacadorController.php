68a7a11d01f6e030b477bc7a46d23012e9bb8101
abielmagu <abielmagu.aio@gmail.com>
2021-10-01 18:44:38 -0500

    Corrige route despues de guardar Reempacador

--- app/Http/Controllers/ReempacadorController.php
+++ app/Http/Controllers/ReempacadorController.php
@@ -27,7 +27,7 @@ class ReempacadorController extends Controller
         if(! Reempacador::create($prepared) )
             return back()->with('failure'. 'Error al guardar reempacador');
 
-        return redirect()->route('reempaque.index')->with('success', 'Reempacador guardado');
+        return redirect()->route('reempacadores.index')->with('success', 'Reempacador guardado');
     }
 
     public function show(Reempacador $reempacador)
