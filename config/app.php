b085094dadfccb186e952772f5b31c8044c715fc
abielmagu <abielmagu.aio@gmail.com>
2021-10-28 04:16:00 -0500

    Implementa rutas, vistas y configuraciones para la autenticacion basica

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Infrastructure
+        App\Providers\FortifyServiceProvider::class,
+
     ],
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -11,71 +11,83 @@
 |
 */
 
-// Escritorio
-Route::get('/', fn() => view('app'))->name('escritorio');
+use Illuminate\Support\Facades\Route;
 
-// Configuracion
-Route::resource('configuraciones', ConfiguracionController::class, [
-    'parameters' => [
-        'configuraciones' => 'configuracion',
-    ],
-])->except(['create','store','destroy']);
 
-// Entradas
-Route::prefix('entradas')->group( function () {
-    // Comentarios
-    Route::post('{entrada}/comentarios', 'ComentarioController@store')->name('comentarios.store');
-    
-    // Imprimir
-    Route::get('{entrada}/imprimir/{guia}', 'EntradaController@imprimir')->name('entradas.imprimir');
-    Route::get('imprimir', 'EntradaController@imprimirMultiple')->name('entradas.imprimir.multiple');
+// Guest routes
+Route::middleware('guest')->group( function () {
 
-    // EntradaEtapa
-    Route::get('{entrada}/etapas/add', 'EntradaEtapaController@add')->name('entradas.etapas.add');
-    Route::get('{entrada}/etapas/{etapa}/edit', 'EntradaEtapaController@edit')->name('entradas.etapas.edit');
-    Route::post('{entrada}/etapas', 'EntradaEtapaController@store')->name('entradas.etapas.store');
-    Route::match(['put','patch'],'{entrada}/etapas/{etapa}', 'EntradaEtapaController@update')->name('entradas.etapas.update');
-    Route::delete('{entrada}/etapas/{etapa}', 'EntradaEtapaController@destroy')->name('entradas.etapas.destroy');
+    // Reempaque
+    Route::get('reempaque', 'ReempaqueController@index')->name('reempaque.index');
+    Route::match(['put','patch'], 'reempaque', 'ReempaqueController@update')->name('reempaque.update');
 });
 
-// Zonas
-Route::resource('etapas/{etapa}/zonas', 'ZonaController')->except(['index', 'show']);
 
-// Consolidados
-Route::get('consolidados/{consolidado}/print', 'ConsolidadoController@printing')->name('consolidados.printing');
+// Auth routes
+Route::middleware('auth')->group( function () {
 
-// Reempaque
-Route::get('reempaque', 'ReempaqueController@index')->name('reempaque.index');
-Route::match(['put','patch'], 'reempaque', 'ReempaqueController@update')->name('reempaque.update');
+    // Escritorio
+    Route::get('/', fn() => view('app'))->name('escritorio');
+    
+    // Configuracion
+    Route::resource('configuraciones', ConfiguracionController::class, [
+        'parameters' => [
+            'configuraciones' => 'configuracion',
+        ],
+    ])->except(['create','store','destroy']);
+    
+    // Entradas
+    Route::prefix('entradas')->group( function () {
+        // Comentarios
+        Route::post('{entrada}/comentarios', 'ComentarioController@store')->name('comentarios.store');
+        
+        // Imprimir
+        Route::get('{entrada}/imprimir/{guia}', 'EntradaController@imprimir')->name('entradas.imprimir');
+        Route::get('imprimir', 'EntradaController@imprimirMultiple')->name('entradas.imprimir.multiple');
+    
+        // EntradaEtapa
+        Route::get('{entrada}/etapas/add', 'EntradaEtapaController@add')->name('entradas.etapas.add');
+        Route::get('{entrada}/etapas/{etapa}/edit', 'EntradaEtapaController@edit')->name('entradas.etapas.edit');
+        Route::post('{entrada}/etapas', 'EntradaEtapaController@store')->name('entradas.etapas.store');
+        Route::match(['put','patch'],'{entrada}/etapas/{etapa}', 'EntradaEtapaController@update')->name('entradas.etapas.update');
+        Route::delete('{entrada}/etapas/{etapa}', 'EntradaEtapaController@destroy')->name('entradas.etapas.destroy');
+    });
+    
+    // Zonas
+    Route::resource('etapas/{etapa}/zonas', 'ZonaController')->except(['index', 'show']);
+    
+    // Consolidados
+    Route::get('consolidados/{consolidado}/print', 'ConsolidadoController@printing')->name('consolidados.printing');
 
-// Registrar
-Route::get('registrar', 'RegistrarController@index')->name('registrar.index');
-Route::post('registrar', 'RegistrarController@update')->name('registrar.update');
-// Route::match(['put','patch'], 'registrar', 'RegistrarController@update')->name('registrar.update');
+    // Registrar
+    Route::get('registrar', 'RegistrarController@index')->name('registrar.index');
+    Route::post('registrar', 'RegistrarController@update')->name('registrar.update');
+    // Route::match(['put','patch'], 'registrar', 'RegistrarController@update')->name('registrar.update');
 
-// Resources
-Route::resources([
-    'alertas' => AlertaController::class,
-    'clientes' => ClienteController::class,
-    'codigosr' => CodigorController::class,
-    'conductores' => ConductorController::class,
-    'consolidados' => ConsolidadoController::class,
-    'destinatarios' => DestinatarioController::class,
-    'entradas' => EntradaController::class,
-    'etapas' => EtapaController::class,
-    'incidentes' => IncidenteController::class,
-    'reempacadores' => ReempacadorController::class,
-    'remitentes' => RemitenteController::class,
-    'salidas' => SalidaController::class,
-    'transportadoras' => TransportadoraController::class,
-    'usuarios' => UserController::class,
-    'vehiculos' => VehiculoController::class,
-    'guias_impresion' => GuiaImpresionController::class,
-], [
-    'parameters' => [
-        'codigosr' => 'codigor',
-        'conductores' => 'conductor',
-        'reempacadores' => 'reempacador',
-        'guias_impresion' => 'guia', 
-    ]
-]);
+    // Resources
+    Route::resources([
+        'alertas' => AlertaController::class,
+        'clientes' => ClienteController::class,
+        'codigosr' => CodigorController::class,
+        'conductores' => ConductorController::class,
+        'consolidados' => ConsolidadoController::class,
+        'destinatarios' => DestinatarioController::class,
+        'entradas' => EntradaController::class,
+        'etapas' => EtapaController::class,
+        'incidentes' => IncidenteController::class,
+        'reempacadores' => ReempacadorController::class,
+        'remitentes' => RemitenteController::class,
+        'salidas' => SalidaController::class,
+        'transportadoras' => TransportadoraController::class,
+        'usuarios' => UserController::class,
+        'vehiculos' => VehiculoController::class,
+        'guias_impresion' => GuiaImpresionController::class,
+    ], [
+        'parameters' => [
+            'codigosr' => 'codigor',
+            'conductores' => 'conductor',
+            'reempacadores' => 'reempacador',
+            'guias_impresion' => 'guia', 
+        ]
+    ]);
+});
