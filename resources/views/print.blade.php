909d6e43c0d3e2b414423fff9a15f3f3755fbd41
abielmagu <abielmagu.aio@gmail.com>
2021-10-28 14:04:20 -0500

    Actualiza view print

--- resources/views/print.blade.php
+++ resources/views/print.blade.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css-bs/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css-bs/bootstrap-icons.css') }}">
     <title>{{ config('app.name') }}</title>
     @yield('style')
 </head>
 <body onload="print()">
-    @yield('header')
-    
     <div>
-        @yield('content')
+        @yield('header')
     </div>
+    
+    @yield('content')
 </body>
 </html>
