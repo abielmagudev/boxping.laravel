81895cf31cbe6bd8997ac8182fbf4a5737aeb0dd
abielmagu <abielmagu.aio@gmail.com>
2021-10-28 13:59:20 -0500

    Reestructura archivos de Bootstrap CSS y JS y configura en la view app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/bs/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bs/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css-bs/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css-bs/bootstrap-icons.css') }}">
     <title>{{ config('app.name') }}</title>
 </head>
 <body class="@yield('classes')">
@@ -27,7 +27,7 @@
         </div>
     
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script> -->
-    <script src="{{ asset('/bs/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js-bs/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 
     @else
