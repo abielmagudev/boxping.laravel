18e4f25162ebaa1ea8f3bd9963c74e8b43124f32
abielmagu <abielmagu.aio@gmail.com>
2021-10-28 13:42:48 -0500

    Reestructura y configura view principal de app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,27 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('bs-css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('bs-css/bootstrap-icons.css') }}">
-    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <link rel="stylesheet" href="{{ asset('css/bs/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bs/bootstrap-icons.css') }}">
     <title>{{ config('app.name') }}</title>
 </head>
-<body>
-    @include('@.layouts.navbar')
-    <br>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm d-none d-lg-block">
-                @include('@.layouts.sidebar')
-            </div>
-            <div class="col-sm col-sm-12 col-lg-10">
-                @include('@.layouts.alerts')
-                @include('@.layouts.errors')
-                @yield('content')
-                <br>
+<body class="@yield('classes')">
+
+    @auth
+        @include('@.layouts.navbar')
+        <br>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm d-none d-lg-block">
+                    @include('@.layouts.sidebar')
+                </div>
+                <div class="col-sm col-sm-12 col-lg-10">
+                    @include('@.layouts.alerts')
+                    @include('@.layouts.errors')
+                    @yield('content')
+                    <br>
+                </div>
             </div>
         </div>
-    </div>
-    @include('@.layouts.scripts')
+    
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script> -->
+    <script src="{{ asset('/bs/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    @else
+        @yield('content')
+
+    @endauth
+
+    @stack('scripts')
+
 </body>
 </html>
