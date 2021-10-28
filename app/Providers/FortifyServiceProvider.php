a6762beb7852f99b555c708a0a15b5ba6650bffd
abielmagu <abielmagu.aio@gmail.com>
2021-10-28 13:41:58 -0500

    Reestructura y configura view login

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -31,7 +31,7 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Fortify::loginView('login');
+        Fortify::loginView('authenticate/login');
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
---
+++ resources/views/authenticate/login.blade.php
@@ -0,0 +1,35 @@
+@extends('app')
+@section('classes', 'bg-primary bg-gradient vh-100')
+@section('content')
+<div class="container-fluid h-100">
+    <div class="row align-items-center justify-content-center h-100">
+        <div class="col-sm col-sm-4">
+            <div class="p-5 bg-white shadow">
+                <p class="lead text-center">Bienvenido</p>
+                <form action="{{ route('login') }}" method="post" autocomplete="off">
+                    @csrf
+                    <div class="form-group mb-3">
+                        <label for="input-email" class="d-none">Email</label>
+                        <input type="email" class="form-control rounded-pill {{ ! $errors->has('email') ?: 'is-invalid' }}" id="input-email" name="email" value="{{ old('email') }}" placeholder="Correo electrónico" autofocus>
+                        @error('email')
+                        <small class="form-text text-danger ms-3">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="input-password" class="d-none">Contraseña</label>
+                        <input type="password" class="form-control rounded-pill {{ ! $errors->has('password') ?: 'is-invalid' }}" id="input-password" name="password" placeholder="Password">
+                        @error('password')
+                        <small class="form-text text-danger ms-3">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary rounded-pill w-100">Iniciar sesión</button>
+                </form>
+                <hr>
+                <p class="text-center">
+                    <a href="{{ route('reempaque.index') }}">Área de reempaque</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
