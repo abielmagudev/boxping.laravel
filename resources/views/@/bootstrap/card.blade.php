191e77846a8d7eea7d63ebba7d745e6d846fe4be
abielmagu <abielmagu.aio@gmail.com>
2021-10-30 05:03:45 -0500

    Actualiza componente y partial

--- resources/views/@/bootstrap/card.blade.php
+++ resources/views/@/bootstrap/card.blade.php
@@ -1,11 +1,17 @@
 <?php 
 
 $settings = (object) [
+    'has_header' => isset($title) || isset($subtitle) || isset($counter) || isset($center) || isset($options),
     'has_body' => $slot->isNotEmpty(),
     'has_footer' => isset($footer),
-    'has_header' => isset($header),
+    'header' => (object) [
+        'title' => $title ?? null,
+        'subtitle' => $subtitle ?? null,
+        'counter' => $counter ?? null,
+        'center' => $center ?? null,
+        'options' => $options ?? null,
+    ],
     'body' => $slot ?? '',
-    'header' => $header ?? '',
     'footer' => $footer ?? '',
 ];
 
@@ -14,7 +20,25 @@ $settings = (object) [
 <div class="card border-0 shadow-sm">
     @if( $settings->has_header )
     <div class="card-header border-bottom bg-white py-3">
-        {!! $settings->header !!}
+        <div class="d-flex justify-content-between align-items-center"> 
+            <div class="text-start">
+                @if( $settings->header->title )
+                <span class="lead align-middle">{!! $settings->header->title !!}</span>
+                @endif
+
+                @if( $settings->header->counter )
+                <span class="badge bg-dark">{!! $settings->header->counter !!}</span>
+                @endif
+
+                @if( $settings->header->subtitle )
+                <span class="d-block small">{!! $settings->header->subtitle !!}</span>
+                @endif
+            </div>
+
+            <div class="text-center">{!! $settings->header->center !!}</div>
+
+            <div class="text-end">{!! $settings->header->options !!}</div>
+        </div>
     </div>
     @endif
 
@@ -25,7 +49,7 @@ $settings = (object) [
     @endif
 
     @if( $settings->has_footer )
-    <div class="card-footer">
+    <div class="card-footer bg-white py-3">
         {!! $settings->footer !!}
     </div>
     @endif
