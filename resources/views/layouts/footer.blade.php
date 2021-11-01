04ed909fc59203695e64b4a56d10c5229648a32b
abielmagu <abielmagu.aio@gmail.com>
2021-11-01 13:49:24 -0500

    Actualiza layout footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,13 @@
-<footer class="py-3">
+<footer class="my-5 text-secondary small">
     <hr>
     <div class="row">
-        <div class="col-sm">
-            {{ config('app.name') }} @ {{ date('Y') }}
+        <div class="col-sm order-last order-md-first">
+            <span>{{ date('Y') }} @Appname</span>
         </div>
-        <div class="col-sm"></div>
+        <div class="col-sm order-first order-md-last text-md-end">
+            <a href="#!" class="link-secondary text-decoration-none">FAQ</a>
+            <a href="#!" class="link-secondary text-decoration-none mx-2">Documentación</a>
+            <a href="#!" class="link-secondary text-decoration-none">Nosotros</a>
+        </div>
     </div>
 </footer>
