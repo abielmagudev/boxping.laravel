5f1105779b57641d2790f665868ac8f0271fd69d
abielmagu <abielmagu.aio@gmail.com>
2021-11-02 04:03:23 -0500

    Actualiza form save de Alerta

--- resources/views/alertas/_save.blade.php
+++ resources/views/alertas/_save.blade.php
@@ -15,7 +15,7 @@
         <div class="row mb-3 mb-md-0">
             <div class="col-sm col-sm-1">
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="nivel" value="{{ $nivel }}" id="radio-nivel-{{ $nivel }}" style="border: 2px solid {{ $attrs['color'] }}" {{ $loop->first ? 'checked' : '' }} {{ toggleChecked($nivel, $alerta->nivel) }}>
+                    <input class="form-check-input" type="radio" name="nivel" value="{{ $nivel }}" id="radio-nivel-{{ $nivel }}" style="border: 2px solid <?= $attrs['color'] ?>" {{ $loop->first ? 'checked' : '' }} {{ toggleChecked($nivel, $alerta->nivel) }}>
                     <label class="form-check-label" for="radio-nivel-{{ $nivel }}">{{ ucfirst($nivel) }}</label>
                 </div>             
             </div>
