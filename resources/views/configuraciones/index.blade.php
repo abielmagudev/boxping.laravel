a33c7823e01968ecb252a30ef91d22bef0835ffb
abielmagu <abielmagu.aio@gmail.com>
2021-11-03 17:15:19 -0500

    Actualiza view de Configuracion

--- resources/views/configuraciones/index.blade.php
+++ resources/views/configuraciones/index.blade.php
@@ -1,19 +1,17 @@
 @extends('app')
 @section('content')
 
-@component('@.bootstrap.page-header', [
-    'title' => 'Configuración',
+@component('@.bootstrap.card', [
+    'title' => 'Configuración'    
 ])
+    <form action="{{ route('configuraciones.update', $configuracion) }}" method="post">
+        @csrf
+        @method('put')
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" name="cliente_alias_numero" value="1" id="checkbox-cliente-alias-numero" onchange="submit()" {{ toggleChecked(1, $configuracion->cliente_alias_numero) }}>
+            <label class="form-check-label" for="checkbox-cliente-alias-numero">Iniciar el número de entrada con el alias del cliente para impresión.</label>
+        </div>
+    </form>
 @endcomponent
-
-<hr>
-<form action="{{ route('configuraciones.update', $configuracion) }}" method="post">
-    @csrf
-    @method('put')
-    <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" name="cliente_alias_numero" value="1" id="checkbox-cliente-alias-numero" onchange="submit()" {{ toggleChecked(1, $configuracion->cliente_alias_numero) }}>
-        <label class="form-check-label" for="checkbox-cliente-alias-numero">Iniciar el número de entrada con el alias del cliente para impresión.</label>
-    </div>
-</form>
 
 @endsection
