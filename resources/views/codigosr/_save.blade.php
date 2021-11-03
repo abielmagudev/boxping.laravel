476cef849e581abf35f3ebdbeaa26deee8192c59
abielmagu <abielmagu.aio@gmail.com>
2021-11-03 16:45:05 -0500

    Actualiza form save de Codigo R

--- resources/views/codigosr/_save.blade.php
+++ resources/views/codigosr/_save.blade.php
@@ -1,9 +1,11 @@
 @csrf
 <div class="mb-3">
     <label for="input-nombre" class="form-label small">Nombre</label>
-    <input type="text" class="form-control" id="input-nombre" name="nombre" value="{{ old('nombre', $codigor->nombre) }}" required>
+    <input type="text" class="form-control {{ bootstrap_isInputInvalid('nombre', $errors) }}" id="input-nombre" name="nombre" value="{{ old('nombre', $codigor->nombre) }}" required>
+    @include('@.bootstrap.invalid-input-message', ['name' => 'nombre'])
 </div>
 <div class="mb-3">
     <label for="textarea-descripcion" class="form-label small">Descripción</label>
-    <textarea cols="30" rows="5" class="form-control" id="textarea-descripcion" name="descripcion">{{ old('descripcion', $codigor->descripcion) }}</textarea>
+    <textarea cols="30" rows="5" class="form-control {{ bootstrap_isInputInvalid('descripcion', $errors) }}" id="textarea-descripcion" name="descripcion">{{ old('descripcion', $codigor->descripcion) }}</textarea>
+    @include('@.bootstrap.invalid-input-message', ['name' => 'descripcion'])
 </div>
