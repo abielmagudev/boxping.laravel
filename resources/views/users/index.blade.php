5647c696781190806766e6873f22d185db73a10f
abielmagu <abielmagu.aio@gmail.com>
2021-11-08 17:58:33 -0600

    Actualiza index de Usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,40 +1,33 @@
 @extends('app')
 @section('content')
 
-@component('@.bootstrap.page-header', [
+@component('@.bootstrap.card', [
     'title' => 'Usuarios',
     'counter' => $users->count(),
 ])
     @slot('options')
     <a href="{{ route('usuarios.create') }}" class="btn btn-sm btn-primary">
-        <span class="d-block d-md-none fw-bold">+</span>
-        <span class="d-none d-md-block">Nuevo usuario</span>
+        <span class="fw-bold">+</span>
     </a>
     @endslot
-@endcomponent
-
-@component('@.bootstrap.card')
-    @slot('body')
-    @component('@.bootstrap.table')
-        @slot('thead', [
-            'Nombre',
-            'Email',
-        ])
-
-        @slot('tbody')
+    @component('@.bootstrap.table', [
+        'thead' =>  ['Nombre','Email'] 
+    ])
         @foreach($users as $user)
-            <tr>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td class="text-end">
-                    <a href="{{ route('usuarios.show', $user) }}" class="btn btn-sm btn-outline-primary">s</a>
-                    <a href="{{ route('usuarios.edit', $user) }}" class="btn btn-sm btn-outline-warning">e</a>
-                </td>
-            </tr>
+        <tr>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td class="text-end">
+                <a href="{{ route('usuarios.show', $user) }}" class="btn btn-sm btn-outline-primary">
+                    @include('@.bootstrap.icon', ['icon' => 'eye'])
+                </a>
+                <a href="{{ route('usuarios.edit', $user) }}" class="btn btn-sm btn-outline-warning">
+                    @include('@.bootstrap.icon', ['icon' => 'pencil-fill'])
+                </a>
+            </td>
+        </tr>
         @endforeach
-        @endslot
     @endcomponent
-    @endslot
 @endcomponent
 
 @endsection
