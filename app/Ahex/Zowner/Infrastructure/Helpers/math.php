10cd0d96bbb719f8c5d6bb794b9d8aacd23087b4
abielmagu <abielmagu.aio@gmail.com>
2021-11-16 06:00:38 -0600

    Actualiza y optimiza math de helpers

--- app/Ahex/Zowner/Infrastructure/Helpers/math.php
+++ app/Ahex/Zowner/Infrastructure/Helpers/math.php
@@ -2,17 +2,21 @@
 
 if( ! function_exists('percentage') )
 {
-    function percentage($total, $count, $roundup = true)
+    function percentage($total, $count, bool $roundup = true)
     {
+        if( $total == 0 || $count == 0 )
+            return 0;
+
         $decimal = ( 0 . $count ) / $total;
         $percent = $decimal * 100;
+        
         return $roundup ? ceil($percent) : floor($percent);
     }
 }
 
 if( ! function_exists('addition') )
 {
-    function addition($total, $add, $float = true)
+    function addition($total, $add, bool $float = true)
     {
         $result = $total + $add;
         return $float ? round($result, 2) : round($result, 0, PHP_ROUND_HALF_UP);
@@ -21,7 +25,7 @@ if( ! function_exists('addition') )
 
 if( ! function_exists('subtraction') )
 {
-    function subtraction($total, $subtract, $float = true)
+    function subtraction($total, $subtract, bool $float = true)
     {
         $result = $total - $subtract;
         return $float ? round($result, 2) : round($result, 0, PHP_ROUND_HALF_DOWN);
