3f713e16efc9d84b3d74f064dbb2ab32fc7b466b
abielmagu <abielmagu.aio@gmail.com>
2021-12-08 00:53:06 -0600

    Crea nuevo partial toggle checkboxes

---
+++ resources/views/@/partials/toggle-checkboxes/trigger.blade.php
@@ -0,0 +1,5 @@
+<span data-bs-toggle="tooltip" data-bs-placement="top" title="Seleccionar todo">
+    <button class="btn btn-sm btn-outline-primary" id="theToggleCheckboxes">
+        {!! $graffiti->design('ui-checks')->svg() !!}
+    </button>
+</span>
