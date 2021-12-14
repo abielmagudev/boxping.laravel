ec03d1cecf157e4f89b281757742c22b46af827b
abielmagu <abielmagu.aio@gmail.com>
2021-12-14 05:31:10 -0600

    Actualiza views de GuiaImpresion

--- resources/views/guias_impresion/_save/formato.blade.php
+++ resources/views/guias_impresion/_save/formato.blade.php
@@ -3,19 +3,19 @@
    <div class="col-sm">
       <?php $formato_ancho = $guia->formato->ancho ?? null ?>
       <label class="form-label small" for="input-formato-ancho">Ancho</label>
-      <input type="number" min="0.01" step="0.01" id="input-formato-ancho" class="form-control {{ bootstrap_isInputInvalid('formato.ancho', $errors) }}" name="formato[ancho]" value="{{ old('formato.ancho', $formato_ancho) }}">
+      <input type="number" min="0.01" step="0.01" id="input-formato-ancho" class="form-control {{ bootstrap_isInputInvalid('formato.ancho', $errors) }}" name="formato[ancho]" value="{{ old('formato.ancho', $formato_ancho) }}" required>
       @include('@.bootstrap.invalid-input-message', ['name' => 'formato[ancho]'])
    </div>
    <div class="col-sm">
       <?php $formato_altura = $guia->formato->altura ?? null ?>
       <label class="form-label small" for="input-formato-altura">Altura</label>
-      <input type="number" min="0.01" step="0.01" id="input-formato-altura" class="form-control {{ bootstrap_isInputInvalid('formato.altura', $errors) }}" name="formato[altura]" value="{{ old('formato.altura', $formato_altura) }}">
+      <input type="number" min="0.01" step="0.01" id="input-formato-altura" class="form-control {{ bootstrap_isInputInvalid('formato.altura', $errors) }}" name="formato[altura]" value="{{ old('formato.altura', $formato_altura) }}" required>
       @include('@.bootstrap.invalid-input-message', ['name' => 'formato[altura]'])
    </div>
    <div class="col-sm">
       <?php $formato_medicion = $guia->formato->medicion ?? null ?>
       <label class="form-label small" for="select-formato-medicion">Medición</label>
-      <select id="select-formato-medicion" class="form-select {{ bootstrap_isInputInvalid('formato.medicion', $errors) }}" name="formato[medicion]">
+      <select id="select-formato-medicion" class="form-select {{ bootstrap_isInputInvalid('formato.medicion', $errors) }}" name="formato[medicion]" required>
          @foreach($pagina->mediciones as $value => $tag)
          <option value="{{ $value }}" {{ toggleSelected($value, old('formato.medicion', $formato_medicion)) }}>{{ ucfirst($tag) }}</option>
          @endforeach
--- resources/views/guias_impresion/_save/notas.blade.php
+++ resources/views/guias_impresion/_save/notas.blade.php
@@ -1,6 +1,6 @@
 <div class="mb-3">
    <label for="input-nombre" class="form-label small">Notas</label>
-   <textarea class="form-control {{ bootstrap_isInputInvalid('nombre', $errors) }}" id="textarea-notas" name="notas">{{ old('notas', $guia->notas) }}</textarea>
+   <input type="text" class="form-control {{ bootstrap_isInputInvalid('nombre', $errors) }}" id="textarea-notas" name="notas" value="<?= old('notas', $guia->notas) ?>">
    <small class="form-text">Si se requiere imprimir información extra al final de la guía de impresión.</small>
    @include('@.bootstrap.invalid-input-message', ['name' => 'notas'])
 </div>
