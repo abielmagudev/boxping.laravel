dac4099ccfa96a98b0f0afcd8834b2c640cdc4ad
abielmagu <abielmagu.aio@gmail.com>
2021-12-14 05:32:16 -0600

    Actualiza y optimiza para guardar el orden de impresion de los contenidos de GuiaImpresion

--- resources/views/guias_impresion/_save/contenido.blade.php
+++ resources/views/guias_impresion/_save/contenido.blade.php
@@ -1,17 +1,23 @@
 <h6>Contenido</h6>
-<div class="d-md-flex border rounded p-3 {{ bootstrap_isInputInvalid('contenido', $errors, 'border-danger') }}">
-   @foreach($contenidos as $name => $model)     
-   <div class="mb-3">
-      <label class="form-label small">{{ ucfirst($name) }}</label>
-      @foreach($model::contentForPrintingGuide() as $attr => $tag)
+<div class="row border rounded mx-1 py-3 {{ bootstrap_isInputInvalid('contenido', $errors, 'border-danger') }}">
+   @foreach($contenidos as $name => $model)
+   <?php $content = $model::contentForPrintingGuide() ?>
+   <div class="col-sm">
+      <label class="form-label fw-bold small">{{ ucfirst($name) }}</label>
+      <div class="input-group input-group-sm mb-3">
+         <span class="input-group-text text-secondary small" id="input-orden">ORDEN</span>
+         <input type="number" class="form-control" step="1" min="1" max="100" name="contenido[<?= $name ?>][orden]" value="<?= $guia->hasContenido($name, 'orden') ? old("contenido.{$name}.orden", $guia->getContenidoOrden($name)) : $loop->iteration ?>" id="input-orden">
+      </div>
+      @foreach($content as $attr => $label)
+      @continue( $attr == 'orden' )     
       <?php
          $checkbox_checked = $guia->hasContenido($name, $attr) || old("contenido.{$name}.{$attr}") === 'yes' ? 'checked' : '';
          $checkbox_name = "contenido[{$name}][{$attr}]";
          $checkbox_id = "checkbox-{$name}-{$attr}";
       ?>
-      <div class="form-check me-5">
-         <input class="form-check-input" type="checkbox" id="{{ $checkbox_id }}" name="{{ $checkbox_name }}" value="yes" {{ $checkbox_checked }}>
-         <label class="form-check-label" for="{{ $checkbox_id }}">{{ $tag }}</label>
+      <div class="form-check">
+         <input class="form-check-input" type="checkbox" id="<?= $checkbox_id ?>" name="<?= $checkbox_name ?>" value="yes" <?= $checkbox_checked ?>>
+         <label class="form-check-label" for="<?= $checkbox_id ?>">{{ $label }}</label>
       </div>
       @endforeach
    </div>
