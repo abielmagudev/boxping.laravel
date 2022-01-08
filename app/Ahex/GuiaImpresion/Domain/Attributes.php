400e41de8ddefbe7c28191ab5979e948d4576ba3
abielmagu <abielmagu.aio@gmail.com>
2022-01-08 02:38:26 -0600

    Actualiza atributos de GuiaImpresion

--- app/Ahex/GuiaImpresion/Domain/Attributes.php
+++ app/Ahex/GuiaImpresion/Domain/Attributes.php
@@ -4,6 +4,7 @@ namespace App\Ahex\GuiaImpresion\Domain;
 
 trait Attributes
 {
+    // Decodes
     public function getFormatoAttribute()
     {
         return json_decode($this->formato_encoded);
@@ -19,39 +20,6 @@ trait Attributes
         return json_decode($this->tipografia_encoded);
     }
 
-    public function getNombreFuenteAttribute()
-    {
-        return ucwords( $this->tipografia->fuente );
-    }
-
-    public function getMedicionTamanoFuenteAttribute()
-    {
-        return $this->tipografia->medicion <> 'pc' ? $this->tipografia->medicion : '%';
-    }
-
-    public function getMedidasFuenteAttribute()
-    {
-        return $this->tipografia->tamano . $this->medicion_tamano_fuente;
-    }
-
-    public function getMedidasPaginaAttribute()
-    {
-        $width  = $this->formato->ancho . $this->formato->medicion;
-        $height = $this->formato->altura . $this->formato->medicion;
-        
-        return  "{$width} {$height}";
-    }
-
-    public function getMargenesPaginaAttribute()
-    {
-        $top    = ! is_null($this->margenes->arriba)    ? $this->margenes->arriba . $this->margenes->medicion : 'auto';
-        $right  = ! is_null($this->margenes->derecha)   ? $this->margenes->derecha . $this->margenes->medicion : 'auto';
-        $bottom = ! is_null($this->margenes->abajo)     ? $this->margenes->abajo . $this->margenes->medicion : 'auto';
-        $left   = ! is_null($this->margenes->izquierda) ? $this->margenes->izquierda . $this->margenes->medicion : 'auto';
-        
-        return "{$top} {$right} {$bottom} {$left}";
-    }
-
     public function getContenidoAttribute()
     {
         return json_decode($this->contenido_encoded);
@@ -62,6 +30,53 @@ trait Attributes
         return json_decode($this->contenido_encoded, true); // get_object_vars( $this->contenido )
     }
 
+
+
+    // Formato attributes
+    public function getMedidasPaginaAttribute()
+    {
+        $width  = $this->formato->ancho . $this->formato->medicion;
+        $height = $this->formato->altura . $this->formato->medicion;
+        
+        return  "{$width} {$height}";
+    }
+
+
+    // Margenes attributes
+    public function getMargenesPaginaAttribute()
+    {
+        $top    = ! is_null($this->margenes->arriba)    ? $this->margenes->arriba . $this->margenes->medicion : 'auto';
+        $right  = ! is_null($this->margenes->derecha)   ? $this->margenes->derecha . $this->margenes->medicion : 'auto';
+        $bottom = ! is_null($this->margenes->abajo)     ? $this->margenes->abajo . $this->margenes->medicion : 'auto';
+        $left   = ! is_null($this->margenes->izquierda) ? $this->margenes->izquierda . $this->margenes->medicion : 'auto';
+        
+        return "{$top} {$right} {$bottom} {$left}";
+    }
+
+
+    // Tipografia attributes
+    public function getNombreFuenteAttribute()
+    {
+        return ucwords( $this->tipografia->fuente );
+    }
+
+    public function getTamanoFuenteAttribute()
+    {
+        return is_float($this->tipografia->tamano) ? (float) $this->tipografia->tamano : (int) $this->tipografia->tamano;
+    }
+
+    public function getMedicionFuenteAttribute()
+    {
+        return $this->tipografia->medicion <> 'pc' ? $this->tipografia->medicion : '%';
+    }
+
+    public function getMedidasFuenteAttribute()
+    {
+        return $this->tamano_fuente . $this->medicion_fuente;
+    }
+
+
+    // Contenido attributes
     public function getContenidoJsonAttribute()
     {
         return $this->contenido_encoded;
