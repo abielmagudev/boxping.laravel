ce9a685bfa4140bd955ffb10fff55130c803e734
abielmagu <abielmagu.aio@gmail.com>
2022-01-12 02:50:37 -0600

    Actualiza views save de GuiaImpresion

--- resources/views/guias_impresion/_save.blade.php
+++ resources/views/guias_impresion/_save.blade.php
@@ -1,8 +1,8 @@
 @csrf
-@includeWhen($guia->isReal(), 'guias_impresion._save.desactivar')
 @include('guias_impresion._save.basico')
 @include('guias_impresion._save.formato')
 @include('guias_impresion._save.margenes')
 @include('guias_impresion._save.tipografia')
 @include('guias_impresion._save.informacion')
 @include('guias_impresion._save.texto_final')
+@includeWhen($guia->isReal(), 'guias_impresion._save.desactivar')
--- resources/views/guias_impresion/_save/desactivar.blade.php
+++ resources/views/guias_impresion/_save/desactivar.blade.php
@@ -1,4 +1,5 @@
 <div class="alert alert-warning">
+    <p class="alert-heading fw-bold">Atención</p>
     <div class="form-check form-switch">
         <input class="form-check-input" type="checkbox" role="switch" id="desactivarSwitchCheckChecked" name="desactivar" value="0" <?= $guia->isActivada() ?: 'checked' ?>>
         <label class="form-check-label" for="desactivarSwitchCheckChecked">Desactivar temporalmente esta guía para evitar impresiones.</label>
