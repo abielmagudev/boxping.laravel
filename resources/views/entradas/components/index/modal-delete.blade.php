2789eaae5837fb18e497a10968c29a6f4db73c81
Abiel Magu <abielmagu.dev@gmail.com>
2022-02-08 03:27:23 -0600

    Actualiza modal delete del componente index de Entrada

--- resources/views/entradas/components/index/modal-delete.blade.php
+++ resources/views/entradas/components/index/modal-delete.blade.php
@@ -2,26 +2,18 @@
 
 include resource_path('views/entradas/components/index/_entradas_form_config.php');
 
-$component = new class()
-{
-    public $all_id = [
-        'button' => 'buttonModalDeleteMultiple',
-        'content' => 'contentModalDeleteMultiple',
-        'counter' => 'counterModalDeleteMultiple',
-        'modal' => 'modalDeleteMultiple',
-    ];
-
-    public function id(string $item)
-    {
-        return $this->all_id[$item];
-    }
-};
+$component = (object) [
+    'trigger_id' => 'modalDeleteMultipleTrigger',
+    'content_id' => 'modalDeleteMultipleContent',
+    'counter_id' => 'modalDeleteMultipleCounter',
+    'modal_id' => 'modalDeleteMultiple',
+];
 
 ?>
 
 @include('@.bootstrap.modal-trigger', [
-    'modal_id' => $component->id('modal'),
-    'data' => ['id' => $component->id('button')],
+    'modal_id' => $component->modal_id,
+    'data' => ['id' => $component->trigger_id],
     'classes' => 'dropdown-item',
     'text' => "<span>{$graffiti->design('x-lg')->svg()}</span>
                 <span class='align-middle ms-1'>Eliminar</span>",
@@ -29,67 +21,64 @@ $component = new class()
 
 @push('modals')
     @component('@.bootstrap.modal', [
-        'id' => $component->id('modal'),
-        'title' => 'Eliminar entradas',
+        'id' => $component->modal_id,
         'header_close' => true,
         'footer_close' => true,
     ])
-    <div class="text-center my-4" id='<?= $component->id('content') ?>'> 
-        <div class="text-danger mb-3">
-            {!! $graffiti->design('exclamation-triangle', ['width' => 104, 'height' => 104])->svg() !!}
-        </div>
-
-        <div class="text-secondary">
-            <div class="h2 mb-4">¿Estás seguro?</div>
-            <div class="px-4">
-                <p>
-                    <span>Se eliminará un total de entradas</span>
-                    <b id="<?= $component->id('counter') ?>"></b>
-                    <br>
-                    <span>no estará disponible para próximas operaciones.</span>
-                </p>
+        <div class="row my-3 px-5" id='<?= $component->content_id ?>'>
+            <div class="col-sm">
+                <div class="text-center text-danger">
+                    {!! $graffiti->design('exclamation-triangle-fill', ['width' => 112, 'height' => 112])->svg() !!}
+                </div>
             </div>
-            <small class="text-danger text-uppercase">* Eliminación permanente, no es recuperable</small>
+            <div class="col-sm">
+                <div class="text-center text-secondary lead">
+                    <p class="mt-2 mb-0">Se eliminarán permanentemente</p>
+                    <p class="h4">
+                        <span id="<?= $component->counter_id ?>"></span>
+                        <span>entradas</span>
+                    </p>
+                </div>
+            </div>
         </div>
 
         @slot('footer')
         <button class="btn btn-danger" type="button" data-entradas-form-action="<?= route('entradas.destroy.multiple') ?>" data-entradas-form-method="delete">Eliminar</button>
         @endslot
-    </div>
     @endcomponent
 @endpush
    
 @push('scripts')
 <script>
-    const buttonModalDeleteMultiple = {
-        trigger: document.getElementById("<?= $component->id('button') ?>"),
-        listening: function () {
-            this.trigger.addEventListener('click', function () {
-                contentModalDeleteMultiple.update()
-            })
-        }
+const modalDeleteMultipleTrigger = {
+    trigger: document.getElementById("<?= $component->trigger_id ?>"),
+    listening: function () {
+        this.trigger.addEventListener('click', function () {
+            modalDeleteMultipleContent.update()
+        })
     }
+}
 
-    const contentModalDeleteMultiple = {
-        element: document.getElementById('<?= $component->id('content') ?>'),
-        counter: document.getElementById('<?= $component->id('counter') ?>'),
-        allCheckboxesEntradas: function () {
-            return document.querySelectorAll('input[type=checkbox][id^=<?= $entradas_form_config->checkbox_prefix ?>]:checked');
-        },
-        countCheckboxesEntradas: function () {
-            return this.allCheckboxesEntradas().length
-        },
-        hasCheckboxesEntradas: function () {
-            return this.countCheckboxesEntradas > 0
-        },
-        updateCounter: function () {
-            this.counter.innerText = this.countCheckboxesEntradas()
-        },
-        update: function () {
-            this.updateCounter()
-        }
+const modalDeleteMultipleContent = {
+    element: document.getElementById('<?= $component->content_id ?>'),
+    counter: document.getElementById('<?= $component->counter_id ?>'),
+    allCheckboxesEntradas: function () {
+        return document.querySelectorAll('input[type=checkbox][id^=<?= $entradas_form_config->checkbox_prefix ?>]:checked');
+    },
+    countCheckboxesEntradas: function () {
+        return this.allCheckboxesEntradas().length
+    },
+    hasCheckboxesEntradas: function () {
+        return this.countCheckboxesEntradas > 0
+    },
+    updateCounter: function () {
+        this.counter.innerText = this.countCheckboxesEntradas()
+    },
+    update: function () {
+        this.updateCounter()
     }
+}
 
-    buttonModalDeleteMultiple.listening()
+modalDeleteMultipleTrigger.listening()
 </script>
 @endpush
