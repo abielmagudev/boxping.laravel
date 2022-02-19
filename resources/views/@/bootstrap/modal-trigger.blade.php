87a2fe6bd7a22414d9429e40224b8834bfa76a93
Abiel Magu <abielmagu.dev@gmail.com>
2022-02-19 04:03:32 -0600

    Refactoriza y actualiza componentes de modal y modal-trigger, requiere actualizar views para su implementacion

--- resources/views/@/bootstrap/modal-trigger.blade.php
+++ resources/views/@/bootstrap/modal-trigger.blade.php
@@ -1,31 +1,55 @@
 <?php
 
-$getDataAttributes = function ($data)
+$settings = [
+    'button' => isset($button) && is_bool($button) ? $button : true,
+    'classes' => isset($classes) && is_string($classes) ? $classes : null,
+    'dataset' => isset($dataset) && is_array($dataset) ? $dataset : [],
+    'modal_id' => isset($modal_id) && is_string($modal_id) ? "#{$modal_id}" : null,
+    'text' => isset($slot) ? $slot : (isset($text) ? $text : null),
+];
+
+$modal_trigger = new class ($settings)
 {
-    $attributes = [];
-    foreach($data as $key => $value)
+    private $settings = [];
+
+    public function __construct($settings)
     {
-        if(! is_numeric($key) )
-            array_push($attributes, "{$key}=\"{$value}\"") ;
+        $this->settings = $settings;
     }
 
-    return implode(' ', $attributes);
-};
+    public function __get($name)
+    {
+        if(! isset($this->settings[$name]) )
+            return '';
 
-$settings = (object) [
-    'classes' => isset($classes) && is_string($classes) ? $classes : null,
-    'dataset' => isset($data) && is_array($data) ? $getDataAttributes($data) : '',
-    'is_link' => isset($is_link) && is_bool($is_link) ? $is_link : false,
-    'modal_id' => isset($modal_id) && is_string($modal_id) ? "#{$modal_id}" : '#modalIdless',
-    'text' => isset($text) ? $text : 'Modal trigger',
-];
+        return $this->settings[$name];
+    }
+
+    public function dataset()
+    {
+        $props = [];
+
+        foreach($this->dataset as $key => $value)
+        {
+            if(! is_numeric($key) )
+                array_push($props, "{$key}=\"{$value}\"") ;
+        }
+
+        return implode(' ', $props);
+    }
+
+    public function isButton()
+    {
+        return $this->button === true;
+    }
+};
 
 ?>
 
-@if( $settings->is_link )
-    <a href="#!" data-bs-toggle="modal" data-bs-target="{{ $settings->modal_id }}" class="{{ $settings->classes }}" {!! $settings->dataset !!}>{!! $settings->text !!}</a>
+@if( $modal_trigger->isButton() )
+<button type="button" data-bs-toggle="modal" data-bs-target="<?= $modal_trigger->modal_id ?>" class="<?= $modal_trigger->classes ?>" <?= $modal_trigger->dataset() ?> >{!! $modal_trigger->text !!}</button>
 
 @else
-    <button type="button" data-bs-toggle="modal" data-bs-target="{{ $settings->modal_id }}" class="{{ $settings->classes }}" {!! $settings->dataset !!}>{!! $settings->text !!}</button>
+<a href="#!" data-bs-toggle="modal" data-bs-target="<?= $modal_trigger->modal_id ?>" class="<?= $modal_trigger->classes ?>" <?= $modal_trigger->dataset() ?> >{!! $modal_trigger->text !!}</a>
 
 @endif
--- resources/views/@/bootstrap/modal.blade.php
+++ resources/views/@/bootstrap/modal.blade.php
@@ -1,68 +1,192 @@
 <?php
 
-$sizes = [
-    'sm' => 'modal-sm', // Small
-    'lg' => 'modal-lg', // Large
-    'xl' => 'modal-xl', // Extra-large
+$settings = [
+    'animation' => isset($animation) && is_bool($animation) ? $animation : false,
+    'backstatic' => isset($backstatic) && is_bool($backstatic) ? $backstatic : false,
+    'centered' => isset($centered) && is_bool($centered) ? $centered : false,
+    'scrollable' => isset($scrollable) && is_bool($scrollable) ? $scrollable : false,
+    'id' => isset($id) && is_string($id) ? $id : null,
+    'size' => isset($size) && is_string($size) ? $size : null,
+    'fullscreen' => isset($fullscreen) && is_string($fullscreen) ? $fullscreen : null,
+
+    'body' => [
+        'classes' => isset($body['classes']) && is_string($body['classes']) ? $body['classes'] : null,
+        'content' => isset($body_content) ? $body_content : null,
+    ],
+
+    'header' => [
+        'button_close' => isset($header['button_close']) && is_bool($header['button_close']) ? $header['button_close'] : true,
+        'classes' => isset($header['classes']) && is_string($header['classes']) ? $header['classes'] : null,
+        'content' => isset($header_content) ? $header_content : null,
+        'title' => isset($header['title']) && is_string($header['title']) ? $header['title'] : null,
+    ],
+
+    'footer' => [
+        'classes' => isset($footer['classes']) && is_string($footer['classes']) ? $footer['classes'] : null,
+        'content' => isset($footer_content) ? $footer_content : null,
+        'button_close' => isset($footer['button_close']) && is_array($footer['button_close']) || $footer['button_close'] === true ? [
+            'classes' => isset($footer['button_close']['classes']) && is_string($footer['button_close']['classes']) ? $footer['button_close']['classes'] : 'btn btn-secondary',
+            'text' => isset($footer['button_close']['text']) && is_string($footer['button_close']['text']) ? $footer['button_close']['text'] : 'Cerrar',
+        ] : null,
+    ],
 ];
 
-$settings = (object) [
-    'backdrop' => isset($backdrop) && $backdrop === true ? 'data-bs-backdrop="static" data-bs-keyboard="false"' : null,
-    'body_classes' => isset($body_classes) && is_string($body_classes) ? $body_classes : '',
-    'body' => $slot ?? null,
-    'centered' => isset($centered) && $centered === true ? 'modal-dialog-centered' : null,
-    'classes' => isset($classes) && is_string($classes) ? $classes : null,
-    'footer_classes' => isset($footer_classes) && is_string($footer_classes) ? $footer_classes : '',
-    'footer' => $footer ?? null,
-    'fullscreen' => isset($fullscreen) && $fullscreen === true ? 'modal-fullscreen' : null,
-    'has_footer_close' => isset($footer_close) && is_bool($footer_close),
-    'has_footer' => isset($footer),
-    'has_header_close' => isset($header_close) && is_bool($header_close),
-    'has_id' => isset($id) && is_string($id),
-    'has_title' => isset($title) && is_string($title),
-    'header_classes' => isset($header_classes) && is_string($header_classes) ? $header_classes : '',
-    'id_label' => "{$id}Label" ?? 'Label',
-    'id' => $id ?? false,
-    'scrollable' => isset($scrollable) && $scrollable === true ? 'modal-dialog-scrollable' : null,
-    'size' => isset($size, $sizes[$size]) ? $sizes[$size] : null,
-    'title' => $title ?? null,
-];
+$modal = new class ($settings)
+{
+    private $fullscreens = [
+        'sm' => 'modal-fullscreen-sm-down', // Below 576px
+        'md' => 'modal-fullscreen-md-down', // Below 768px
+        'lg' => 'modal-fullscreen-lg-down', // Below 992px
+        'xl' => 'modal-fullscreen-xl-down', // Below 1200px
+        'xxl' => 'modal-fullscreen-xxl-down', // Below 1400px
+        'always' => 'modal-fullscreen',
+    ];
+
+    private $sizes = [
+        'sm' => 'modal-sm', // Small
+        'lg' => 'modal-lg', // Large
+        'xl' => 'modal-xl', // Extra large
+    ];
+
+    private $settings = [];
+
+    public function __construct(array $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    public function __get(string $name)
+    {
+        if(! $this->has($name) )
+            return;
+
+        return $this->settings[$name];
+    }
+
+    public function has(string $setting)
+    {
+        return isset($this->settings[$setting]);
+    }
+
+    public function id(string $postfix = null)
+    {
+        return isset($postfix) && is_string($postfix) ? $this->id . $postfix : $this->id;
+    }
+
+    public function animation()
+    {
+        return $this->animation ? 'fade' : '';
+    }
+
+    public function backstatic()
+    {
+        return $this->backstatic ? 'data-bs-backdrop="static" data-bs-keyboard="false"' : '';
+    }
+
+    public function centered()
+    {
+        return $this->centered ? 'modal-dialog-centered' : '';
+    }
+
+    public function scrollable()
+    {
+        return $this->scrollable ? 'modal-dialog-scrollable' : '';
+    }
+
+    public function fullscreen()
+    {
+        if(! $this->has('fullscreen') ||! array_key_exists($this->fullscreen, $this->fullscreens) )
+            return '';
+
+        return $this->fullscreens[ $this->fullscreen ];
+    }
+
+    public function size()
+    {
+        if(! $this->has('size') )
+            return '';
+
+        return array_key_exists($this->size, $this->sizes) ? $this->sizes[ $this->size ] : '';
+    }
+
+    public function header(string $key)
+    {
+        if(! isset( $this->header[$key] ) )
+            return '';
+
+        return $this->header[$key];
+    }
+
+    public function body(string $key)
+    {
+        if(! isset( $this->body[$key] ) )
+            return '';
+
+        return $this->body[$key];
+    }
+
+    public function footer(string $key, string $subkey = null)
+    {
+        if( isset($subkey, $this->footer[$key][$subkey]) )
+            return $this->footer[$key][$subkey];
+
+        if( isset($this->footer[$key]) && is_null($subkey) )
+            return $this->footer[$key];
+
+        return '';
+    }
+
+    public function hasHeader()
+    {
+        return $this->header('close') === true ||! empty($this->header('title')) ||! empty($this->header('content'));
+    }
+
+    public function hasBody()
+    {
+        return ! empty($this->body('content'));
+    }
+
+    public function hasFooter()
+    {
+        return $this->footer('button_close') ||! empty($this->footer('content'));
+    }
+}
 
 ?>
 
-@if( $settings->has_id )
-<div class="modal fade" tabindex="-1" aria-hidden="true" id="{{ $settings->id }}" aria-labelledby="{{ $settings->id_label }}" {!! $settings->backdrop !!}>
-    <div class="modal-dialog {{ $settings->fullscreen }} {{ $settings->size }} {{ $settings->centered }} {{ $settings->scrollable }}">
+@if( $modal->has('id') )
+<div class="modal fade" tabindex="-1" aria-hidden="true" id="<?= $modal->id() ?>" aria-labelledby="<?= $modal->id('Label') ?>" <?= $modal->backstatic() ?>>
+    <div class="modal-dialog <?= $modal->size() ?> <?= $modal->centered() ?> <?= $modal->scrollable() ?> <?= $modal->fullscreen() ?>">
         <div class="modal-content">
-
-            @if( $settings->has_title || $settings->has_header_close )             
-            <div class="modal-header {{ $settings->header_classes }}">
-                @if( $settings->has_title )
-                <div class="modal-title" id="{{ $settings->id_label }}">{!! $settings->title !!}</div>
+            @if( $modal->hasHeader() )             
+            <div class="modal-header border-0 <?= $modal->header('classes') ?>">
+                @if( $modal->header('title') )
+                <h5 class="modal-title" id="<?= $modal->id('Label') ?>">{!! $modal->header('title') !!}</h5>
                 @endif
 
-                @if( $settings->has_header_close )
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                {!! $modal->header('content') !!}
+
+                @if( $modal->header('button_close') )
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
                 @endif
             </div>
             @endif
 
-            <div class="modal-body {{ $settings->body_classes }}">
-                {!! $settings->body !!}
+            @if( $modal->hasBody() )             
+            <div class="modal-body <?= $modal->body('classes') ?>">
+                {!! $modal->body('content') !!}
             </div>
+            @endif
             
-            @if( $settings->has_footer || $settings->has_footer_close )
-            <div class="modal-footer {{ $settings->footer_classes }}">
-                @if( $settings->has_footer )
-                {!! $settings->footer !!}
-                @endif
+            @if( $modal->hasFooter() )
+            <div class="modal-footer <?= $modal->footer('classes') ?>">
+                {!! $modal->footer('content') !!}
 
-                @if( $settings->has_footer_close ) 
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                @if( is_array($modal->footer('button_close')) ) 
+                <button type="button" class="<?= $modal->footer('button_close', 'classes') ?>" data-bs-dismiss="modal">{{ $modal->footer('button_close', 'text') }}</button>
                 @endif
             </div>
             @endif
-
         </div>
     </div>
 </div>
