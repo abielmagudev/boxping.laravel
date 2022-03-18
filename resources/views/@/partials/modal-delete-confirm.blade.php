79600ecffecf01bf31c58ebe3e6d129d4927526b
Abiel Magu <abielmagu.dev@gmail.com>
2022-03-18 18:33:37 -0500

    Refactoriza y optimiza modal para confirmacion de eliminar

---
+++ resources/views/@/partials/modal-delete-confirm.blade.php
@@ -0,0 +1,60 @@
+<?php
+
+$modal = (object) [
+    'trigger' => [
+        'text' => $button['text'] ?? 'Eliminar',
+        'classes' => $button['classes'] ?? 'link-danger',
+    ],
+    'form' => [
+        'id' => 'formConfirmDelete',
+        'content' => $form ?? null,
+        'route' => $route,
+    ],
+    'destroy' => isset($destroy) && is_bool($destroy) ? $destroy : false, 
+    'id' => 'modalDeleteConfirm',
+    'message' => $slot ?? '',
+];
+
+?>
+
+@component('@.bootstrap.modal-trigger', [
+    'classes' => $modal->trigger['classes'],
+    'modal_id' => $modal->id,
+    'button' => false
+])
+    <span>{{ $modal->trigger['text'] }}</span>
+@endcomponent
+
+@push('modals')
+    @component('@.bootstrap.modal', [
+        'id' => $modal->id,
+        'header' => [
+            'classes' => 'bg-danger text-white'
+        ],
+        'footer' => [
+            'button_close' => [
+                'text' => 'Cancelar'
+            ],
+        ],
+    ])
+        @slot('body_content')
+        <div class="text-center mt-5 px-4">
+            <div>{{ $modal->message }}</div>
+            <form action="<?= $modal->form['route'] ?>" method="post" id="<?= $modal->form['id'] ?>">
+                @csrf
+                @method('delete')
+                {{ $modal->form['content'] }}
+            </form>
+
+            @if( $modal->destroy )
+            <p class="text-danger text-uppercase small mt-5">Eliminación permanente, no es recuperable</p>
+            @endif
+        </div>
+
+        @endslot
+
+        @slot('footer_content')
+        <button class="btn btn-outline-danger" type="button" form="<?= $modal->form['id'] ?>">Eliminar</button>
+        @endslot
+    @endcomponent
+@endpush
