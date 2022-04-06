ebbd1a47653fcea4f9f9a38e8a91d1bb2bcf5b31
Abiel Magu <abielmagu.dev@gmail.com>
2022-04-05 22:18:09 -0500

    Agrega scope para index de Entrada

--- app/Ahex/Entrada/Domain/Scopes.php
+++ app/Ahex/Entrada/Domain/Scopes.php
@@ -24,6 +24,22 @@ trait Scopes
         return $query->with(['comentarios.creator']);
     }
 
+    public function scopeWithIndex($query, $attach = null)
+    {
+        $index = ['consolidado', 'cliente', 'destinatario'];
+
+        if( isset($attach) )
+        {
+            if( is_array($attach) && count($attach) > 0 )
+                $index = array_merge($index, array_values($attach));
+    
+            if( is_string($attach) &&! empty( trim($attach) ) )
+                array_push($index, $attach);
+        }
+
+        return $query->with($index);
+    }
+
     public function scopeWithRelations($query)
     {
         return $query->with([
