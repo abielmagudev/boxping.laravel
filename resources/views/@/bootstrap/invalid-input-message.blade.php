cef5243c5edf39e21a7b390b79cc5d5000931b7e
Abiel Magu <abielmagu.dev@gmail.com>
2022-04-26 01:57:33 -0500

    Actualiza componente de validacion de formulario

--- resources/views/@/bootstrap/invalid-input-message.blade.php
+++ resources/views/@/bootstrap/invalid-input-message.blade.php
@@ -1,3 +1,3 @@
-@error($name)
-<div class="invalid-feedback" role="alert">{{ $message }}</div>
+@error($name, $rule = null)
+<div class="text-danger small" role="alert">{!! $message !!}</div>
 @enderror
