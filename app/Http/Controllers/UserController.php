98f9b00da0c5f85bbc67cb8a847348028ea2b14b
Abiel Magu <abielmagu.dev@gmail.com>
2022-04-30 09:06:18 -0500

    Bind model en show de Usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -52,7 +52,7 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $usuario)
     {
         return redirect()->route('usuarios.index');
     }
