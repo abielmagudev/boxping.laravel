81ac231788884c13690bbd68ac7dc15f3003262b
Abiel Magu <abielmagu.dev@gmail.com>
2022-05-03 00:08:50 -0500

    Elimina partial checkboxes-toggle

--- resources/views/@/partials/checkboxes-toggle/trigger.blade.php
+++
@@ -1,5 +0,0 @@
-<span data-bs-toggle="tooltip" data-bs-placement="top" title="Seleccionar todo">
-    <button class="btn btn-sm btn-outline-primary" id="theToggleCheckboxes">
-        {!! $graffiti->design('ui-checks')->svg() !!}
-    </button>
-</span>
