331255ea46a5eaf50f88d8c36607fc097b99701c
Abiel Magu <abielmagu.dev@gmail.com>
2022-05-03 00:08:24 -0500

    Agrega salidas a settings del componente index de Salida

--- resources/views/salidas/index.blade.php
+++ resources/views/salidas/index.blade.php
@@ -1,7 +1,11 @@
 @extends('app')
 @section('content')
 
-@include('salidas.index.card')
+@include('salidas.index.card', [
+    'settings' => [
+        'salidas' => $salidas
+    ],
+])
 @include('salidas.index.pagination')
 
 @endsection
